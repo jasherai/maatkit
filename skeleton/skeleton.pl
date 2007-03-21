d2b744a258f5823c676d67ae820fc468379eb91e
bps7j <bps7j>
2007-03-21 22:09:08 +0000

    I discovered the DBD::mysql documentation

--- skeleton/skeleton.pl
+++ skeleton/skeleton.pl
@@ -39,27 +39,29 @@ our $VERSION = '@VERSION@';
 # Define cmdline args; each is GetOpt::Long spec, whether required,
 # human-readable description.  Add more hash entries as needed.
 my %opt_spec = (
-   d => { s => 'database|d=s', r => 0, d => 'Database' },
-   h => { s => 'host|h=s',     r => 0, d => 'Database server hostname' },
+   D => { s => 'database|D=s', r => 0, d => 'Database to use' },
+   h => { s => 'host|h=s',     r => 0, d => 'Connect to host' },
    l => { s => 'help',         r => 0, d => 'Show this help message' },
-   o => { s => 'port|P=i',     r => 0, d => 'Database server port' },
+   P => { s => 'port|P=i',     r => 0, d => 'Database server port' },
+   S => { s => 'socket|S=s',   r => 0, d => 'Socket file to use for connection' },
    p => { s => 'pass|p=s',     r => 0, d => 'Database password' },
-   u => { s => 'user|u=s',     r => 0, d => 'Database username' },
+   u => { s => 'user|u=s',     r => 0, d => 'User for login if not current user' },
 );
 
 # Define the order cmdline opts will appear in help output.  Add any extra ones
 # defined above.  If it's not in this list, it's not an option to this
 # program.  Note that 'h' is host and 'l' is help.
-my @opt_keys = qw( h d o u p l );
+my @opt_keys = qw( D h p P S u l );
 
 # This is the container for the command-line options' values to be stored in
 # after processing.  Initial values are defaults.
 my %opts = (
-   d => '',
-   h => '',
-   o => 3306,
+   D => undef,
+   h => undef,
+   P => undef,
    p => undef,
    u => undef,
+   S => undef,
 );
 
 Getopt::Long::Configure('no_ignore_case', 'bundling');
@@ -89,64 +91,16 @@ USAGE
    exit(1);
 }
 
-# This will end up containing what's needed to connect to MySQL.
-my $conn = {
-   h  => $opts{h},
-   db => $opts{d},
-   u  => $opts{u},
-   p  => $opts{p},
-   o  => $opts{o},
-};
-
-if ( grep { !$conn->{$_} } keys %$conn ) {
-   # Try to use the user's .my.cnf file.
-   eval {
-      my $homedir = $ENV{HOME} || $ENV{HOMEPATH} || $ENV{USERPROFILE};
-      open my $conf_file, "<", "$homedir/.my.cnf" or die $OS_ERROR;
-      while ( my $line = <$conf_file> ) {
-         chomp $line;
-         $line =~ s/(^\s*)|(\s*#.*$)//g;
-         next unless $line;
-         my ( $key, $val ) = split( /\s*=\s*/, $line );
-         next unless defined $val;
-         if ( $key eq 'host' )     { $conn->{h}  ||= $val; }
-         if ( $key eq 'user' )     { $conn->{u}  ||= $val; }
-         if ( $key =~ m/^pass/ )   { $conn->{p}  ||= $val; }
-         if ( $key eq 'database' ) { $conn->{db} ||= $val; }
-         if ( $key eq 'port' )     { $conn->{o}  ||= $val; }
-      }
-      close $conf_file;
-   };
-   if ( $EVAL_ERROR && $EVAL_ERROR !~ m/No such file/ ) {
-      print "I tried to read your .my.cnf file, but got '$EVAL_ERROR'\n";
-   }
-}
-
-# Fill in defaults for some things
-$conn->{h} ||= 'localhost';
-$conn->{u} ||= getlogin() || getpwuid($UID);
-
-my %prompts = (
-   o  => "Port number: ",
-   h  => "Database host: ",
-   u  => "Database user: ",
-   p  => "Database password: ",
-   db => "Database: ",
-);
-
-# If anything remains, prompt the terminal
-my $term;
-foreach my $thing ( grep { !defined $conn->{$_} } keys %$conn ) {
-   $term ||= Term::ReadLine->new('terminal');
-   $conn->{$thing} = $term->readline($prompts{$thing});
-}
-
 # ############################################################################
 # Get ready to do the main work.
 # ############################################################################
+my %conn = ( h => 'host', P => 'port', S => 'socket');
 
 # Connect to the database
-my $dbh = DBI->connect(
-   "DBI:mysql:database=$conn->{db};host=$conn->{h};port=$conn->{o}",
-   $conn->{u}, $conn->{p}, { AutoCommit => 1, RaiseError => 1, PrintError => 1 } )
+my $dsn = 'DBI:mysql:' . ( $opts{D} || '' ) . ';'
+   . join(';', map  { "$conn{$_}=$opts{$_}" } grep { defined $opts{$_} } qw(h P S))
+   . ';mysql_read_default_group=mysql';
+   print $dsn, "\n";
+   exit;
+my $dbh = DBI->connect($dsn, @opts{qw(u p)}, { AutoCommit => 1, RaiseError => 1, PrintError => 1 } )
    or die("Can't connect to DB: $OS_ERROR");
