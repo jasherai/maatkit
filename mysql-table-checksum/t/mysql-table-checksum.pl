d86ce85eabfd47d5550bb66af8ad7eac4459114b
bps7j <bps7j>
2007-05-05 18:42:06 +0000

    Change default behavior for --count to avoid extra COUNT(*) query. Reorganize documentation on --algorithm and --function. Add more tests for --count.

--- mysql-table-checksum/t/mysql-table-checksum.pl
+++ mysql-table-checksum/t/mysql-table-checksum.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More tests => 27;
+use Test::More tests => 30;
 
 my $opt_file = shift or die "Specify an option file.\n";
 diag("Testing with $opt_file");
@@ -17,8 +17,8 @@ my $output = `perl ../mysql-table-checksum --defaults-file=$opt_file -d test -t 
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
 
-my ( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+) *(\w+)/;
-is ( $cnt, 2, 'There are two rows in the table' );
+my ( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
 }
@@ -35,8 +35,8 @@ $output = `perl ../mysql-table-checksum -a ACCUM --defaults-file=$opt_file -d te
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
 
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+) *(\w+)/;
-is ( $cnt, 2, 'There are two rows in the table' );
+( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
 }
@@ -51,8 +51,8 @@ else {
 $output = `perl ../mysql-table-checksum -a BIT_XOR --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+) *(\w+)/;
-is ( $cnt, 2, 'There are two rows in the table' );
+( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
 }
@@ -70,8 +70,8 @@ else {
 $output = `perl ../mysql-table-checksum -a BIT_XOR -f MD5 --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+) *(\w+)/;
-is ( $cnt, 2, 'There are two rows in the table' );
+( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to ACCUM/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5/ACCUM is okay' );
 }
@@ -88,8 +88,8 @@ my $result = `mysql --defaults-file=$opt_file --skip-column-names -e "select thi
 chomp $result;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+) *(\w+)/;
-is ( $cnt, 2, 'There are two rows in the table' );
+( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
 }
@@ -100,6 +100,13 @@ else {
    is ( $crc, 'c590354a59c8c9bec447696046580bc47d83f922', 'SHA1 is okay' );
 }
 is ( $crc, $result, 'output matches what was in the table' );
+
+# Test --count functionality
+$output = `perl ../mysql-table-checksum --count --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
+like($output, qr/^DATABASE/m, 'The header row is there');
+like($output, qr/checksum_test/, 'The results row is there');
+( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+is ( $cnt, 2, 'There are two rows in the table' );
 
 # Test that two tables with same contents produce same result
 $output = `perl ../mysql-table-checksum --defaults-file=$opt_file -d test -t checksum_test,checksum_test_2 127.0.0.1 2>&1`;
