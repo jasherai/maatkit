b944e37de4d6c2d2c80f04e5ff50187817558f5f
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-05-16 02:08:16 +0000

    Update test suite for chunks and make regression test for --optxor. Add current/total slice to comments in checksum queries. BIT_XOR slicing algorithm was not setting user var correctly with --optxor so it was always disabled.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@403 dfb901c2-3250-0410-b216-0b33211c9131

--- mysql-table-checksum/t/mysql-table-checksum.pl
+++ mysql-table-checksum/t/mysql-table-checksum.pl
@@ -3,21 +3,22 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More tests => 30;
+use Test::More tests => 35;
 
 my $opt_file = shift or die "Specify an option file.\n";
+my ($output, $output2);
 diag("Testing with $opt_file");
 
 # Load.
 `mysql --defaults-file=$opt_file < before.sql`;
 
 # Test basic functionality with defaults
-my $output = `perl ../mysql-table-checksum --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
+$output = `perl ../mysql-table-checksum --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
 
-my ( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+my ( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
@@ -35,7 +36,7 @@ $output = `perl ../mysql-table-checksum -a ACCUM --defaults-file=$opt_file -d te
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
 
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
@@ -51,7 +52,7 @@ else {
 $output = `perl ../mysql-table-checksum -a BIT_XOR --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
@@ -70,7 +71,7 @@ else {
 $output = `perl ../mysql-table-checksum -a BIT_XOR -f MD5 --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to ACCUM/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5/ACCUM is okay' );
@@ -83,12 +84,12 @@ else {
 }
 
 # Test --replicate functionality
-$output = `perl ../mysql-table-checksum -R test.checksums --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
-my $result = `mysql --defaults-file=$opt_file --skip-column-names -e "select this_crc from test.checksums where tbl='checksum_test'"`;
+$output = `perl ../mysql-table-checksum -R test.checksum --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
+my $result = `mysql --defaults-file=$opt_file --skip-column-names -e "select this_crc from test.checksum where tbl='checksum_test'"`;
 chomp $result;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 like ( $cnt, qr/2|NULL/, 'There are either two rows in the table, or no count' );
 if ( $output =~ m/falling back to MD5/ ) {
    is ( $crc, '68415d6c42e35059e0c30a2bc334b4a5', 'MD5 is okay' );
@@ -105,7 +106,7 @@ is ( $crc, $result, 'output matches what was in the table' );
 $output = `perl ../mysql-table-checksum --count --defaults-file=$opt_file -d test -t checksum_test 127.0.0.1 2>&1`;
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
-( $cnt, $crc ) = $output =~ m/checksum_test \S+ \S+ *(\d+|NULL) *(\w+)/;
+( $cnt, $crc ) = $output =~ m/checksum_test *\d+ \S+ \S+ *(\d+|NULL) *(\w+)/;
 is ( $cnt, 2, 'There are two rows in the table' );
 
 # Test that two tables with same contents produce same result
@@ -119,3 +120,12 @@ $output = `perl ../mysql-table-checksum -a BIT_XOR --defaults-file=$opt_file -d 
 like($output, qr/^DATABASE/m, 'The header row is there');
 like($output, qr/checksum_test/, 'The results row is there');
 like($output, qr/ (\d+ [\w]+).*\1/ms,  'There are two identical lines');
+
+# Test that --optxor and --nooptxor produce the same result
+$output  = `perl ../mysql-table-checksum -a BIT_XOR --optxor --defaults-file=$opt_file -d test -t checksum_test_2 127.0.0.1 2>&1`;
+$output2 = `perl ../mysql-table-checksum -a BIT_XOR --nooptxor --defaults-file=$opt_file -d test -t checksum_test_2 127.0.0.1 2>&1`;
+like($output, qr/^DATABASE/m, 'The header row is there');
+like($output, qr/checksum_test/, 'The results row is there');
+like($output2, qr/^DATABASE/m, 'The header row is there');
+like($output2, qr/checksum_test/, 'The results row is there');
+is($output, $output2, "The two BIT_XOR methods gave the same results");
