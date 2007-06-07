5aee34c490ee8e68a8453b5958d1d4125ed74232
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-06-07 01:37:02 +0000

    jail --where clause in parens, make --where required, don't accept extra cmdline options
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@513 dfb901c2-3250-0410-b216-0b33211c9131

--- mysql-archiver/t/mysql-archiver.pl
+++ mysql-archiver/t/mysql-archiver.pl
@@ -16,65 +16,65 @@ $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.tabl
 is($output + 0, 4, 'Test data loaded ok');
 
 # Test --forupdate
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --forupdate --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --forupdate --purge 2>&1`;
 like($output, qr/SELECT .*? FOR UPDATE/, 'forupdate works');
 
 # Test --sharelock
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --sharelock --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --sharelock --purge 2>&1`;
 like($output, qr/SELECT .*? LOCK IN SHARE MODE/, 'sharelock works');
 
 # Test --quickdel
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --quickdel --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --quickdel --purge 2>&1`;
 like($output, qr/DELETE QUICK/, 'quickdel works');
 
 # Test --lpdel
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --lpdel --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --lpdel --purge 2>&1`;
 like($output, qr/DELETE LOW_PRIORITY/, 'lpdel works');
 
 # Test --lpins
-$output = `perl ../mysql-archiver -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --lpins 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --lpins 2>&1`;
 like($output, qr/INSERT LOW_PRIORITY/, 'lpins works');
 
 # Test --delayedins
-$output = `perl ../mysql-archiver -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --delayedins 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --delayedins 2>&1`;
 like($output, qr/INSERT DELAYED/, 'delayedins works');
 
 # Test --replace
-$output = `perl ../mysql-archiver -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --replace 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --replace 2>&1`;
 like($output, qr/REPLACE/, 'replace works');
 
 # Test --hpselect
-$output = `perl ../mysql-archiver --hpselect -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --replace 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --hpselect -t -d t=table_2 --source D=test,t=table_1,F=$opt_file --replace 2>&1`;
 like($output, qr/SELECT HIGH_PRIORITY/, 'hpselect works');
 
 # Test basic functionality with defaults
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 is($output, '', 'Basic test run did not die');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged ok');
 
 # Test basic functionality with --commit-each
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --commit-each --limit 1 --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --commit-each --limit 1 --purge 2>&1`;
 is($output, '', 'Commit-each did not die');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged ok with --commit-each');
 
 # Test basic functionality with OPTIMIZE
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver -O ds --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -O ds --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 is($output, '', 'OPTIMIZE did not fail');
 
 # Test an empty table
 `mysql --defaults-file=$opt_file < before.sql`;
 $output = `mysql --defaults-file=$opt_file -N -e "delete from test.table_1"`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 is($output, "", 'Empty table OK');
 
 # Test with a sentinel file
 `mysql --defaults-file=$opt_file < before.sql`;
 `touch sentinel`;
-$output = `perl ../mysql-archiver --sentinel sentinel --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --sentinel sentinel --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 is($output, "", 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 4, 'No rows were deleted');
@@ -83,36 +83,36 @@ is($output + 0, 4, 'No rows were deleted');
 # Test ascending index (it should ascend the primary key, but there is
 # no way to really know; I just want to make sure it doesn't die)
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_3,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_3,F=$opt_file --purge 2>&1`;
 is($output, '', 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_3"`;
 is($output + 0, 0, 'Ascended key OK');
 
 # Test specifying a wrong index.
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source i=foo,D=test,t=table_3,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source i=foo,D=test,t=table_3,F=$opt_file --purge 2>&1`;
 is($output, "The specified index could not be found, or there is no PRIMARY key.\n", 'Got bad-index error OK');
 
 # Test specifying a NULLable index.
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source i=b,D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source i=b,D=test,t=table_1,F=$opt_file --purge 2>&1`;
 is($output, "Some columns in index `b` allow NULL.\n", 'Got NULL-index error');
 
 # Test table without a primary key
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_4,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_4,F=$opt_file --purge 2>&1`;
 is($output, "The source table does not have a primary key.  Cannot continue.\n", 'Got need-PK-error OK');
 
 # Test ascending index explicitly
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_3,F=$opt_file,i=PRIMARY --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_3,F=$opt_file,i=PRIMARY --purge 2>&1`;
 is($output, '', 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_3"`;
 is($output + 0, 0, 'Ascended explicit key OK');
 
 # Archive only part of the table
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --where 'a<4' --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --where 'a<4' --purge 2>&1`;
 is($output, '', 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 1, 'Purged some rows ok');
@@ -120,7 +120,7 @@ is($output + 0, 1, 'Purged some rows ok');
 # Archive to a file.
 `mysql --defaults-file=$opt_file < before.sql`;
 `rm -f archive.test.table_1`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --file 'archive.%D.%t' 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --file 'archive.%D.%t' 2>&1`;
 is($output, '', 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged all rows ok');
@@ -138,7 +138,7 @@ EOF
 
 # Archive to another table.
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
 is($output, '', 'No output');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged all rows ok');
@@ -147,7 +147,7 @@ is($output + 0, 4, 'Found rows in new table OK');
 
 # Test the output
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver --source D=test,t=table_1,F=$opt_file --purge --progress 2 2>&1 | awk '{print \$3}'`;
+$output = `perl ../mysql-archiver -W 1=1 --source D=test,t=table_1,F=$opt_file --purge --progress 2 2>&1 | awk '{print \$3}'`;
 is($output, <<EOF
 COUNT
 0
@@ -159,7 +159,7 @@ EOF
 
 # Archive to another table with autocommit
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver -z 0 --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -z 0 --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
 is($output, '', 'Commit every 0 rows worked OK');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged all rows ok');
@@ -168,7 +168,7 @@ is($output + 0, 4, 'Found rows in new table OK');
 
 # Archive to another table with commit every 2 rows
 `mysql --defaults-file=$opt_file < before.sql`;
-$output = `perl ../mysql-archiver -z 2 --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -z 2 --source D=test,t=table_1,F=$opt_file --dest t=table_2 2>&1`;
 is($output, '', 'Commit every 2 rows worked OK');
 $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.table_1"`;
 is($output + 0, 0, 'Purged all rows ok');
@@ -176,17 +176,17 @@ $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.tabl
 is($output + 0, 4, 'Found rows in new table OK');
 
 # Test --columns
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --columns=a,b --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --columns=a,b --purge 2>&1`;
 like($output, qr/SELECT (?:SQL_NO_CACHE)? `a`,`b` FROM/, 'Only got specified columns');
 
 # Test --pkonly
-$output = `perl ../mysql-archiver -t --source D=test,t=table_1,F=$opt_file --pkonly --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --source D=test,t=table_1,F=$opt_file --pkonly --purge 2>&1`;
 like($output, qr/SELECT (?:SQL_NO_CACHE)? `a` FROM/, '--pkonly works');
 
 # Test that tables must have same columns
-$output = `perl ../mysql-archiver -t --dest t=table_4 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --dest t=table_4 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 like($output, qr/The following columns exist in --source /, 'Column check throws error');
-$output = `perl ../mysql-archiver -t --nochkcols --dest t=table_4 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --nochkcols --dest t=table_4 --source D=test,t=table_1,F=$opt_file --purge 2>&1`;
 like($output, qr/SELECT/, 'I can disable the check OK');
 
 # Test that ascending index check WHERE clause can't be hijacked
@@ -202,9 +202,9 @@ $output = `mysql --defaults-file=$opt_file -N -e "select count(*) from test.tabl
 is($output + 0, 0, 'Purged completely');
 
 # Make sure ascending index check can be disabled
-$output = `perl ../mysql-archiver -t --noascend -s D=test,t=table_5,F=$opt_file -p -l 50 2>&1`;
+$output = `perl ../mysql-archiver -W 1=1 -t --noascend -s D=test,t=table_5,F=$opt_file -p -l 50 2>&1`;
 like ( $output, qr/(^SELECT .*$)\n\1/m, '--noascend makes fetch-first and fetch-next identical' );
 
 # Check ascending only first column
-$output = `perl ../mysql-archiver -t --ascendfirst -s D=test,t=table_5,F=$opt_file -p -l 50 2>&1`;
-like ( $output, qr/WHERE \(\(`a` >= \?\)\) LIMIT/, 'Can ascend just first column');
+$output = `perl ../mysql-archiver -W 1=1 -t --ascendfirst -s D=test,t=table_5,F=$opt_file -p -l 50 2>&1`;
+like ( $output, qr/WHERE \(1=1\) AND \(`a` >= \?\) LIMIT/, 'Can ascend just first column');
