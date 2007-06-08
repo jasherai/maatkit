2fd3eb7e5b98ac6c7bed20525facf06a0327b07d
bps7j <bps7j>
2007-06-08 17:33:41 +0000

    tests for plugin functionality

---
+++ mysql-archiver/t/Plugin2.pm
@@ -0,0 +1,13 @@
+package Plugin2;
+
+sub new {
+   my ( $class, %args ) = @_;
+   return bless(\%args, $class);
+}
+
+sub is_archivable {1} # Always yes
+sub before_delete {} # Take no action
+sub before_insert {} # Take no action
+
+1;
+
