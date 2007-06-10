271e24f44809a843976eefbdd592f385f30a02f8
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-06-10 21:18:54 +0000

    make archives
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@544 dfb901c2-3250-0410-b216-0b33211c9131

--- mysqltoolkit/package.pl
+++ mysqltoolkit/package.pl
@@ -28,33 +28,38 @@ print `svn ci -m 'Bump version' packlist`;
 my $rev = `head -n 1 packlist | awk '{print \$2}'` + 0;
 
 # make the dist directory
-`rm -rf dist`;
-`mkdir -p dist/bin dist/lib`;
+my $dist = "mysqltoolkit-$rev";
+`find -type d -name 'mysqltoolkit-*' | xargs rm -rf`;
+`mkdir -p $dist/bin $dist/lib`;
 
 # Write mysqltoolkit.pod
 # TODO: include the NAME section from each one's POD between the 'mid' and
 # 'tail' files
-print `cat mysqltoolkit.head.pod packlist mysqltoolkit.mid.pod mysqltoolkit.tail.pod > dist/lib/mysqltoolkit.pm`;
+print `cat mysqltoolkit.head.pod packlist mysqltoolkit.mid.pod mysqltoolkit.tail.pod > $dist/lib/mysqltoolkit.pm`;
 
-# Copy the executables and their READMEs into the dist dir, and set the
+# Copy the executables and their READMEs into the $dist dir, and set the
 # $VERSION variable correctly
 foreach my $p ( keys %versions ) {
    my ($version) = $versions{$p} =~ m/([\d.]+)/;
-   print `for a in $base/$p/$versions{$p}/mysql-*; do b=\`basename \$a\`; cp \$a dist/bin; sed -i -e 's/\@VERSION\@/$version/' dist/bin/\$b; done`;
-   `cp $base/$p/$versions{$p}/README dist/README.$p`;
+   print `for a in $base/$p/$versions{$p}/mysql-*; do b=\`basename \$a\`; cp \$a $dist/bin; sed -i -e 's/\@VERSION\@/$version/' $dist/bin/\$b; done`;
+   `cp $base/$p/$versions{$p}/README $dist/README.$p`;
 }
 
 # Set the DISTRIB variable
-`grep DISTRIB -rl dist | xargs sed -i -e 's/\@DISTRIB\@/$rev/'`;
+`grep DISTRIB -rl $dist | xargs sed -i -e 's/\@DISTRIB\@/$rev/'`;
 
 # Copy other files
 foreach my $file ( qw(Makefile.PL COPYING INSTALL) ) {
-   `cp $file dist`;
+   `cp $file $dist`;
 }
 
 # Write the MANIFEST
-`find dist -type f -print | sed -e 's/dist.//' > dist/MANIFEST`;
-`echo MANIFEST >> dist/MANIFEST`;
+`find $dist -type f -print | sed -e 's/$dist.//' > $dist/MANIFEST`;
+`echo MANIFEST >> $dist/MANIFEST`;
+
+# Do it!!!!
+print `tar zcf $dist.tar.gz $dist`;
+print `zip -r $dist.zip $dist`;
 
 # #####################################################################
 # Subroutines
