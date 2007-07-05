7ed53250f721866e4061326a41056fcdc49e189f
bps7j <bps7j>
2007-07-05 18:23:59 +0000

    fix some small errors

--- mysqltoolkit/package.pl
+++ mysqltoolkit/package.pl
@@ -26,14 +26,15 @@ open my $packlist, "> packlist" or die $!;
 print $packlist '   $Revision$', "\n", map { "   $_\n" } sort values %versions;
 close $packlist;
 print `svn ci -m 'Bump version' packlist`;
-`rm packlist`; `svn revert packlist`; # Just in case there were no changes
+print `rm packlist`;
+print `svn revert packlist`; # Just in case there were no changes
 my $rev = `head -n 1 packlist | awk '{print \$2}'` + 0;
 
 # make the dist directory
 my $distbase = "mysqltoolkit-$rev";
 my $dist = "release/$distbase";
-`rm -rf release html cache`;
-`mkdir -p html cache $dist/bin $dist/lib`;
+print `rm -rf release html cache`;
+print `mkdir -p html cache $dist/bin $dist/lib`;
 
 # Copy the executables and their READMEs into the $dist dir, and set the
 # $VERSION variable correctly
@@ -42,7 +43,7 @@ foreach my $p ( keys %versions ) {
    print `for a in $base/$p/$versions{$p}/mysql-*; do b=\`basename \$a\`; cp \$a $dist/bin; sed -i -e 's/\@VERSION\@/$version/' $dist/bin/\$b; done`;
    `cp $base/$p/$versions{$p}/README $dist/README.$p`;
 }
-`cp README $dist/`;
+print `cp README $dist/`;
 
 # Write mysqltoolkit.pod
 print `cat mysqltoolkit.head.pod packlist mysqltoolkit.mid.pod > $dist/lib/mysqltoolkit.pm`;
@@ -60,15 +61,15 @@ print `cat mysqltoolkit.tail.pod >> $dist/lib/mysqltoolkit.pm`;
 
 # Copy other files
 foreach my $file ( qw(Makefile.PL COPYING INSTALL mysqltoolkit.spec) ) {
-   `cp $file $dist`;
+   print `cp $file $dist`;
 }
 
 # Set the DISTRIB variable
-`grep DISTRIB -rl $dist | xargs sed -i -e 's/\@DISTRIB\@/$rev/'`;
+print `grep DISTRIB -rl $dist | xargs sed -i -e 's/\@DISTRIB\@/$rev/'`;
 
 # Write the MANIFEST
-`find $dist -type f -print | sed -e 's/$dist.//' > $dist/MANIFEST`;
-`echo MANIFEST >> $dist/MANIFEST`;
+print `find $dist -type f -print | sed -e 's~$dist.~~' > $dist/MANIFEST`;
+print `echo MANIFEST >> $dist/MANIFEST`;
 
 # Do it!!!!
 print `cd release && tar zcf $distbase.tar.gz $distbase`;
@@ -103,10 +104,10 @@ for ( 0 .. 1 ) {
    );
 }
 # Wow, Pod::HTML is a pain in the butt.  Fix links now.
-`for a in html/*; do sed -i -e 's~bin/~~g' \$a; done`;
+print `for a in html/*; do sed -i -e 's~bin/~~g' \$a; done`;
 
 # Cleanup temporary directories
-`rm -rf cache $dist`;
+print `rm -rf cache $dist`;
 
 # #####################################################################
 # Subroutines
