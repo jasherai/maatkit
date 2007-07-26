3c056c8a8c90346db2cf4119c8ddc6e683b5fbae
bps7j <bps7j>
2007-07-26 03:02:20 +0000

    work on subqueries

--- mysql-visual-explain/t/samples/nested_derived_tables.sql
+++ mysql-visual-explain/t/samples/nested_derived_tables.sql
@@ -3,23 +3,17 @@ from (
    select *, (select count(*) from sakila.film as mid_sub where mid_sub.film_id = mid_der.film_id)
    from (
       select *, (select count(*) from sakila.film as inner_sub where inner_sub.film_id = inner_der.film_id)
-      from (
-         select *, (select count(*) from sakila.film as inmost_sub where inmost_sub.film_id = inmost_der.film_id)
-         from sakila.film as inmost_der
-      ) as inner_der
+      from sakila.film as inner_der
    ) as mid_der
 ) as outer_der;
 
-+----+--------------------+------------+--------+---------------+---------+---------+--------------------+------+--------------------------+
-| id | select_type        | table      | type   | possible_keys | key     | key_len | ref                | rows | Extra                    |
-+----+--------------------+------------+--------+---------------+---------+---------+--------------------+------+--------------------------+
-|  1 | PRIMARY            | <derived3> | ALL    | NULL          | NULL    | NULL    | NULL               | 1000 |                          | 
-|  3 | DERIVED            | <derived5> | ALL    | NULL          | NULL    | NULL    | NULL               | 1000 |                          | 
-|  5 | DERIVED            | <derived7> | ALL    | NULL          | NULL    | NULL    | NULL               | 1000 |                          | 
-|  7 | DERIVED            | inmost_der | ALL    | NULL          | NULL    | NULL    | NULL               | 1131 |                          | 
-|  8 | DEPENDENT SUBQUERY | inmost_sub | eq_ref | PRIMARY       | PRIMARY | 2       | inmost_der.film_id |    1 | Using where; Using index | 
-|  6 | DEPENDENT SUBQUERY | inner_sub  | eq_ref | PRIMARY       | PRIMARY | 2       | inner_der.film_id  |    1 | Using where; Using index | 
-|  4 | DEPENDENT SUBQUERY | mid_sub    | eq_ref | PRIMARY       | PRIMARY | 2       | mid_der.film_id    |    1 | Using where; Using index | 
-|  2 | DEPENDENT SUBQUERY | outer_sub  | eq_ref | PRIMARY       | PRIMARY | 2       | outer_der.film_id  |    1 | Using where; Using index | 
-+----+--------------------+------------+--------+---------------+---------+---------+--------------------+------+--------------------------+
-8 rows in set (0.12 sec)
++----+--------------------+------------+--------+---------------+---------+---------+-------------------+------+--------------------------+
+| id | select_type        | table      | type   | possible_keys | key     | key_len | ref               | rows | Extra                    |
++----+--------------------+------------+--------+---------------+---------+---------+-------------------+------+--------------------------+
+|  1 | PRIMARY            | <derived3> | ALL    | NULL          | NULL    | NULL    | NULL              | 1000 |                          | 
+|  3 | DERIVED            | <derived5> | ALL    | NULL          | NULL    | NULL    | NULL              | 1000 |                          | 
+|  5 | DERIVED            | inner_der  | ALL    | NULL          | NULL    | NULL    | NULL              |  951 |                          | 
+|  6 | DEPENDENT SUBQUERY | inner_sub  | eq_ref | PRIMARY       | PRIMARY | 2       | inner_der.film_id |    1 | Using where; Using index | 
+|  4 | DEPENDENT SUBQUERY | mid_sub    | eq_ref | PRIMARY       | PRIMARY | 2       | mid_der.film_id   |    1 | Using where; Using index | 
+|  2 | DEPENDENT SUBQUERY | outer_sub  | eq_ref | PRIMARY       | PRIMARY | 2       | outer_der.film_id |    1 | Using where; Using index | 
++----+--------------------+------------+--------+---------------+---------+---------+-------------------+------+--------------------------+
