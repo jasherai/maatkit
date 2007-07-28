5b8b854f4232f3170de7397881ea0cfdfc3f3a63
bps7j <bps7j>
2007-07-28 20:36:53 +0000

    got the last bit of recursion into build_query_plan

--- mysql-visual-explain/t/samples/adjacent_subqueries.sql
+++ mysql-visual-explain/t/samples/adjacent_subqueries.sql
@@ -1,4 +1,10 @@
-mysql> explain select (select count(*) from sakila.film_actor where sakila.film_actor.actor_id = sakila.actor.actor_id) + (select count(*) from sakila.film as f) as count_actors from sakila.actor;
+explain
+select (
+   select count(*) from sakila.film_actor
+      where sakila.film_actor.actor_id = sakila.actor.actor_id
+) + (
+   select count(*) from sakila.film as f
+) as count_actors from sakila.actor;
 +----+--------------------+------------+-------+---------------+--------------------+---------+----------------+------+--------------------------+
 | id | select_type        | table      | type  | possible_keys | key                | key_len | ref            | rows | Extra                    |
 +----+--------------------+------------+-------+---------------+--------------------+---------+----------------+------+--------------------------+
@@ -6,6 +12,3 @@ mysql> explain select (select count(*) from sakila.film_actor where sakila.film_
 |  3 | SUBQUERY           | f          | index | NULL          | idx_fk_language_id | 1       | NULL           |  951 | Using index              | 
 |  2 | DEPENDENT SUBQUERY | film_actor | ref   | PRIMARY       | PRIMARY            | 2       | actor.actor_id |   13 | Using where; Using index | 
 +----+--------------------+------------+-------+---------------+--------------------+---------+----------------+------+--------------------------+
-3 rows in set (0.00 sec)
-
-mysql> notee
