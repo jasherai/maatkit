f97e588f83a869b40f794ebbef8cf7214eb6b486
bps7j <bps7j>
2007-08-08 02:48:39 +0000

    fix test

--- mysql-table-sync/t/mysql-table-sync.pl
+++ mysql-table-sync/t/mysql-table-sync.pl
@@ -9,7 +9,7 @@ use Test::More tests => 1;
 
 # Ensure wacky collations and callbacks to MySQL to compare collations don't
 # cause problems.
-my $output = `mysql-table-sync --print -a bottomup baron\@localhost/test.test1 baron\@localhost/test.test2`;
-my $expected = "DELETE FROM `test`.`test2` WHERE `a` = '2' AND `b` = 'é';\n"
+my $output = `../mysql-table-sync --print -a bottomup D=test,t=test1 t=test2`;
+my $expected = "DELETE FROM `test`.`test2` WHERE (`a` = '2' AND `b` = 'é');\n"
              . "INSERT INTO `test`.`test2`(`a`,`b`) VALUES('2','ca');\n";
 is($output, $expected, "Funny characters got synced okay");
