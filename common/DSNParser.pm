f8561edef41c2fa5f1d903be6d1f691b46836890
bps7j <bps7j>
2007-10-20 13:58:44 +0000

    DSN parser module

---
+++ common/DSNParser.pm
@@ -0,0 +1,93 @@
+# ###########################################################################
+# DSNParser package
+# ###########################################################################
+use strict;
+use warnings FATAL => 'all';
+
+package DSNParser;
+
+# Defaults are built-in, but you can add more items by passing them as hashrefs
+# of {key, desc, copy}.  The 'desc' item is optional.
+sub new {
+   my ( $class, @opts ) = @_;
+   my $self = {
+      D => {
+         desc => 'Database to use',
+         dsn  => 'database',
+         copy => 1,
+      },
+      F => {
+         desc => 'Only read default options from the given file',
+         dsn  => 'mysql_read_default_file',
+         copy => 1,
+      },
+      h => {
+         desc => 'Connect to host',
+         dsn  => 'host',
+         copy => 1,
+      },
+      p => {
+         desc => 'Password to use when connecting',
+         dsn  => 'password',
+         copy => 1,
+      },
+      P => {
+         desc => 'Port number to use for connection',
+         dsn  => 'port',
+         copy => 1,
+      },
+      S => {
+         desc => 'Socket file to use for connection',
+         dsn  => 'mysql_socket',
+         copy => 1,
+      },
+      u => {
+         desc => 'User for login if not current user',
+         dsn  => 'user',
+         copy => 1,
+      },
+   };
+   foreach my $opt ( @opts ) {
+      $self->{$opt->{key}} = { desc => $opt->{desc}, copy => $opt->{copy} };
+   }
+   return bless $self, $class;
+}
+
+sub parse {
+   my ( $self, $dsn, $prev, $defaults ) = @_;
+   return unless $dsn;
+   $prev     ||= {};
+   $defaults ||= {};
+   my %hash    = map { m/^(.)=(.*)$/g } split(/,/, $dsn);
+   my %vals;
+   foreach my $key ( keys %$self ) {
+      $vals{$key} = $hash{$key};
+      if ( !defined $vals{$key} && defined $prev->{$key} && $self->{$key}->{copy} ) {
+         $vals{$key} = $prev->{$key};
+      }
+      if ( !defined $vals{$key} ) {
+         $vals{$key} = $defaults->{$key};
+      }
+   }
+   return \%vals;
+}
+
+sub usage {
+   my ( $self ) = @_;
+   my $usage
+      = "  DSN syntax: key=value[,key=value...] Allowable DSN keys:\n"
+      . "  KEY  MEANING\n"
+      . "  ===  =============================================\n";
+   foreach my $key ( sort keys %$self ) {
+      $usage .= "  $key    "
+             .  ($self->{$key}->{desc} || '[No description]')
+             . "\n";
+   }
+   return $usage;
+}
+
+1;
+
+# ###########################################################################
+# End DSNParser package
+# ###########################################################################
---
+++ common/t/DSNParser.t
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 4;
+use English qw(-no_match_vars);
+
+require "../DSNParser.pm";
+
+my $p = new DSNParser;
+
+is_deeply(
+   $p->parse('u=a,p=b'),
+   {  u => 'a',
+      p => 'b',
+      S => undef,
+      h => undef,
+      P => undef,
+      F => undef,
+      D => undef,
+   },
+   'Basic DSN works'
+);
+
+$p = new DSNParser(
+   { key => 't', copy => 0 }
+   );
+
+is_deeply(
+   $p->parse('u=a,p=b'),
+   {  u => 'a',
+      p => 'b',
+      S => undef,
+      h => undef,
+      P => undef,
+      F => undef,
+      D => undef,
+      t => undef,
+   },
+   'DSN works with an extra option'
+);
+
+is_deeply(
+   $p->parse('u=a,p=b', { D => 'foo', h => 'me' }, { S => 'bar', h => 'host' } ),
+   {  D => 'foo',
+      F => undef,
+      h => 'me',
+      p => 'b',
+      P => undef,
+      S => 'bar',
+      t => undef,
+      u => 'a',
+   },
+   'DSN works with defaults'
+);
+
+is ($p->usage(),
+<<EOF
+  DSN syntax: key=value[,key=value...] Allowable DSN keys:
+  KEY  MEANING
+  ===  =============================================
+  D    Database to use
+  F    Only read default options from the given file
+  P    Port number to use for connection
+  S    Socket file to use for connection
+  h    Connect to host
+  p    Password to use when connecting
+  t    [No description]
+  u    User for login if not current user
+EOF
+, 'Usage is OK');
