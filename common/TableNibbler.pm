b7ed113d6553bb7e240ce33bf63effc8487a7332
bps7j <bps7j>
2007-10-25 20:14:23 +0000

    finished

--- common/TableNibbler.pm
+++ common/TableNibbler.pm
@@ -29,6 +29,28 @@ sub sort_indexes {
    return @indexes;
 }
 
+sub find_best_index {
+   my ( $self, $tbl, $index ) = @_;
+   my $best;
+   if ( $index ) {
+      ($best) = grep { uc $_ eq uc $index } keys %{$tbl->{keys}};
+   }
+   if ( !$best ) {
+      if ( $index ) {
+         # The user specified an index, so we can't choose our own.
+         die "Index '$index' does not exist in table";
+      }
+      else {
+         # Try to pick the best index.
+         ($best) = $self->sort_indexes($tbl);
+         if ( !$best ) {
+            die "Cannot find an ascendable index in table";
+         }
+      }
+   }
+   return $best;
+}
+
 # Arguments are as follows:
 # * tbl      Hashref as provided by TableParser.
 # * cols     Arrayref of columns to SELECT from the table. Defaults to all.
@@ -52,24 +74,7 @@ sub generate_asc_stmt {
    # ##########################################################################
    # Detect indexes and columns needed.
    # ##########################################################################
-   # Make sure the lettercase is right and verify that the index exists.
-   my $index;
-   if ( $opts{index} ) {
-      ($index) = grep { uc $_ eq uc $opts{index} } keys %{$tbl->{keys}};
-   }
-   if ( !$index ) {
-      if ( $opts{index} ) {
-         # The user specified an index, so we can't choose our own.
-         die "Index '$opts{index}' does not exist in table";
-      }
-      else {
-         # Try to pick the best index.
-         ($index) = $self->sort_indexes($tbl);
-         if ( !$index ) {
-            die "Cannot find an ascendable index in table";
-         }
-      }
-   }
+   my $index = $self->find_best_index($tbl, $opts{index});
 
    # These are the columns we'll ascend.
    @asc_cols = @{$tbl->{keys}->{$index}->{cols}};
@@ -161,30 +166,45 @@ sub generate_asc_stmt {
    return $asc_stmt;
 }
 
-1;
-
-# Figure out how to delete rows. DELETE requires a PK, a unique non-NULL
-# index, or all columns.  For that reason you should call this before calling
+# Figure out how to delete rows. DELETE requires either an index or all
+# columns.  For that reason you should call this before calling
 # generate_asc_stmt(), so you know what columns you'll need to fetch from the
-# table.
+# table.  Arguments:
+# * tbl * cols * quoter * index
+# These are the same as the arguments to generate_asc_stmt().
 sub generate_del_stmt {
    my ( $self, %opts ) = @_;
 
    my $tbl  = $opts{tbl};
-   my @cols = $opts{cols} ? @{$opts{cols}} : @{$tbl->{cols}};
-   my $idx  = (!$opts{index} || uc $opts{index} eq 'PRIMARY') ? 'PRIMARY' : $opts{index};
+   my @cols = $opts{cols} ? @{$opts{cols}} : ();
    my $q    = $opts{quoter};
 
+   my @del_cols;
+   my @del_slice;
+
    # ##########################################################################
-   # Detect the best or preferred index to use for the WHERE clause.
+   # Detect the best or preferred index to use for the WHERE clause needed to
+   # delete the rows.
    # ##########################################################################
-   # Make sure the lettercase is right and verify that the index exists.
-   my $index = $idx;
-   if ( $idx ne 'PRIMARY' ) {
-      ($index) = grep { uc $_ eq uc $idx } keys %{$tbl->{keys}};
+   my $index = $self->find_best_index($tbl, $opts{index});
+
+   # These are the columns needed for the DELETE statement's WHERE clause.
+   if ( $index ) {
+      @del_cols = @{$tbl->{keys}->{$index}->{cols}};
    }
-   if ( !$index || !$tbl->{keys}->{$index} ) {
-      # TODO
+   else {
+      @del_cols = @{$tbl->{cols}};
+   }
+
+   # We found the columns by name, now find their positions for use as
+   # array slices, and make sure they are included in the SELECT list.
+   my %col_posn = do { my $i = 0; map { $_ => $i++ } @cols };
+   foreach my $col ( @del_cols ) {
+      if ( !exists $col_posn{$col} ) {
+         push @cols, $col;
+         $col_posn{$col} = $#cols;
+      }
+      push @del_slice, $col_posn{$col};
    }
 
    my $del_stmt = {
@@ -195,143 +215,63 @@ sub generate_del_stmt {
       scols => [],
    };
 
+   # ##########################################################################
+   # Figure out how to target a single row with a WHERE clause.
+   # ##########################################################################
+   my @clauses;
+   foreach my $i ( 0 .. $#del_slice ) {
+      my $ord = $del_slice[$i];
+      my $col = $cols[$ord];
+      my $quo = $q->quote($col);
+      if ( $tbl->{is_nullable}->{$col} ) {
+         push @clauses, "((? IS NULL AND $quo IS NULL) OR ($quo = ?))";
+         push @{$del_stmt->{slice}}, $ord, $ord;
+         push @{$del_stmt->{scols}}, $col, $col;
+      }
+      else {
+         push @clauses, "$quo = ?";
+         push @{$del_stmt->{slice}}, $ord;
+         push @{$del_stmt->{scols}}, $col;
+      }
+   }
+
+   $del_stmt->{where} = '(' . join(' AND ', @clauses) . ')';
+
    return $del_stmt;
 }
 
-1;
+# Design an INSERT statement.  This actually does very little; it just maps
+# the columns you know you'll get from the SELECT statement onto the columns
+# in the INSERT statement, returning only those that exist in both sets.
+# Arguments:
+# * tbl * cols
+# These are the same as the arguments to generate_asc_stmt().
+sub generate_ins_stmt {
+   my ( $self, %opts ) = @_;
 
-__DATA__
+   my $tbl  = $opts{tbl};
+   my @cols = @{$opts{cols}};
 
-   # These are the columns we'll ascend.
-   @asc_cols = @{$tbl->{keys}->{$index}->{cols}};
-   if ( $opts{ascfirst} ) {
-      @asc_cols = $asc_cols[0];
-   }
+   die "You didn't specify any columns" unless @cols;
 
-   # We found the columns by name, now find their positions for use as
-   # array slices, and make sure they are included in the SELECT list.
-   my %col_posn = do { my $i = 0; map { $_ => $i++ } @cols };
-   foreach my $col ( @asc_cols ) {
-      if ( !exists $col_posn{$col} ) {
-         push @cols, $col;
-         $col_posn{$col} = $#cols;
+   # Find column positions for use as array slices.
+   my %col_posn = do { my $i = 0; map { $_ => $i++ } @{$tbl->{cols}} };
+   my @ins_cols;
+   my @ins_slice;
+
+   foreach my $col ( @cols ) {
+      if ( exists $col_posn{$col} ) {
+         push @ins_cols, $col;
+         push @ins_slice, $col_posn{$col};
       }
-      push @asc_slice, $col_posn{$col};
    }
 
-   # ##########################################################################
-   # Figure out how to ascend the index by building a possibly complicated
-   # WHERE clause that will define a range beginning with a row retrieved by
-   # asc_stmt.  If asconly is given, the row's lower end should not include
-   # the row.
-   # Assuming a non-NULLable two-column index, the WHERE clause should look
-   # like this:
-   # WHERE (col1 > ?) OR (col1 = ? AND col2 >= ?)
-   # Ascending-only and nullable require variations on this.  The general
-   # pattern is (>), (= >), (= = >), (= = = >=).
-   # ##########################################################################
-   if ( @asc_slice ) {
-      my @clauses;
-      foreach my $i ( 0 .. $#asc_slice ) {
-         my @clause;
-
-         # Most of the clauses should be strict equality.
-         foreach my $j ( 0 .. $i - 1 ) {
-            my $ord = $asc_slice[$j];
-            my $col = $cols[$ord];
-            my $quo = $q->quote($col);
-            if ( $tbl->{is_nullable}->{$col} ) {
-               push @clause, "((? IS NULL AND $quo IS NULL) OR ($quo = ?))";
-               push @{$asc_stmt->{slice}}, $ord, $ord;
-               push @{$asc_stmt->{scols}}, $col, $col;
-            }
-            else {
-               push @clause, "$quo = ?";
-               push @{$asc_stmt->{slice}}, $ord;
-               push @{$asc_stmt->{scols}}, $col;
-            }
-         }
-
-         # The last clause in each parenthesized group should be >, but the
-         # very last of the whole WHERE clause should be >=, unless we are
-         # ascending strictly, in which case everything must be strictly >.
-         my $ord = $asc_slice[$i];
-         my $col = $cols[$ord];
-         my $quo = $q->quote($col);
-         my $end = $i == $#asc_slice; # Last clause of the whole group.
-         if ( $tbl->{is_nullable}->{$col} ) {
-            if ( !$opts{asconly} && $end ) {
-               push @clause, "(? IS NULL OR $quo >= ?)";
-            }
-            else {
-               push @clause, "((? IS NULL AND $quo IS NOT NULL) OR ($quo > ?))";
-            }
-            push @{$asc_stmt->{slice}}, $ord, $ord;
-            push @{$asc_stmt->{scols}}, $col, $col;
-         }
-         else {
-            push @{$asc_stmt->{slice}}, $ord;
-            push @{$asc_stmt->{scols}}, $col;
-            push @clause, (!$opts{asconly} && $end ? "$quo >= ?" : "$quo > ?");
-         }
-
-         # Add the clause to the larger WHERE clause.
-         push @clauses, '(' . join(' AND ', @clause) . ')';
-      }
-      $asc_stmt->{where} = '(' . join(' OR ', @clauses) . ')';
-   }
-
-   return $asc_stmt;
-=pod
-   if ( $tbl->{keys}->{PRIMARY} ) {
-      @pk_slice = map {
-         $src->{info}->{col_posn}->{$_}
-      } @{$src->{info}->{keys}->{PRIMARY}->{cols}};
-      die "Can't find ordinal position of all columns"
-         if grep { !defined($_) } @pk_slice;
-   }
-   else {
-      # At this time, issues with NULLs (=null vs. IS NULL) will prevent DELETEs
-      # from working right without a PRIMARY key.
-      die "The source table does not have a primary key.  Cannot continue.\n";
-      @pk_slice = (0 .. $#cols);
-   }
-
-   $del_sql = 'DELETE'
-      . ($opts{lpdel}    ? ' LOW_PRIORITY' : '')
-      . ($opts{quickdel} ? ' QUICK'        : '')
-      . " FROM $src->{db_tbl} WHERE "
-      . join(' AND ', map { "`$cols[$_]` = ?" } @pk_slice)
-      . " LIMIT 1";
-
-   # INSERT is all columns.  I can't think of why you'd want to archive to a
-   # table with different columns than the source.
-   if ( $dst ) {
-      $ins_sql = ($opts{r}          ? 'REPLACE'       : 'INSERT')
-               . ($opts{lpins}      ? ' LOW_PRIORITY' : '')
-               . ($opts{delayedins} ? ' DELAYED'      : '')
-               . ($opts{i}          ? ' IGNORE'       : '');
-      $ins_sql .= " INTO $dst->{db_tbl}("
-         . join(",", map { "`$_`" } @cols)
-         . ") VALUES ("
-         . join(",", map { "?" } @cols)
-         . ")";
-   }
-   else {
-      $ins_sql = '';
-   }
-
-   if ( $opts{t} ) {
-      print join("\n", ($opts{f} || ''), $first_sql, $next_sql, $del_sql, $ins_sql), "\n";
-      exit(0);
-   }
-=cut
-
-   return {
-      asc_stmt => $asc_stmt,
-      del_stmt => $del_stmt,
+   my $ins_stmt = {
+      cols  => \@ins_cols,
+      slice => \@ins_slice,
    };
 
+   return $ins_stmt;
 }
 
 1;
