bb8cdcc851a722f4fcb6f3b030b0b606fcd78e22
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-10-28 04:13:40 +0000

    declarative parsing begins
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@1126 dfb901c2-3250-0410-b216-0b33211c9131

--- common/OptionParser.pm
+++ common/OptionParser.pm
@@ -8,6 +8,7 @@ package OptionParser;
 
 use Getopt::Long;
 use List::Util qw(max);
+use English qw(-no_match_vars);
 
 # Holds command-line options.  Each option is a hashref:
 # {
@@ -21,19 +22,53 @@ use List::Util qw(max);
 # * l is the option's long name
 # * t is the option's short name
 # * n is whether the option is negatable
-# Returns the options as a hashref.
+# * r is whether the option is required
+# Returns the options as a hashref.  Options can also be plain-text
+# instructions, and instructions are recognized inside the 'd' as well.
 sub new {
    my ( $class, @opts ) = @_;
    my %opt_seen;
+   my %key_for;
+   my @mutex;
    foreach my $opt ( @opts ) {
-      my ( $long, $short ) = $opt->{s} =~ m/^([\w-]+)(?:\|([^!+=]*))?/;
-      $opt->{k} = $short || $long;
-      $opt->{l} = $long;
-      $opt->{t} = $short;
-      $opt->{n} = $opt->{s} =~ m/!/;
-      die "Duplicate option $opt->{k}" if $opt_seen{$opt->{k}}++;
+      if ( ref $opt ) {
+         my ( $long, $short ) = $opt->{s} =~ m/^([\w-]+)(?:\|([^!+=]*))?/;
+         $opt->{k} = $short || $long;
+         $key_for{$long} = $opt->{k};
+         $opt->{l} = $long;
+         $opt->{t} = $short;
+         $opt->{n} = $opt->{s} =~ m/!/;
+         # Instructions in the 'd' description.
+         $opt->{r} = $opt->{d} =~ m/required/;
+         # TODO die on dupe long opts too
+         die "Duplicate option $opt->{k}" if $opt_seen{$opt->{k}}++;
+      }
+      else { # It's an instruction.
+
+         if ( $opt =~ m/are mutually exclusive/ ) {
+            my @participants;
+            foreach my $thing ( $opt =~ m/(--?\S+)/g ) {
+               if ( (my ($long) = $thing =~ m/--(\S+)/) ) {
+                  push @participants, $key_for{$long}
+                     or die "No such option $thing";
+               }
+               else {
+                  foreach my $short ( $thing =~ m/([^-])/g ) {
+                     push @participants, $key_for{$short};
+                  }
+               }
+            }
+            push @mutex, \@participants;
+         }
+
+      }
    }
-   return bless { specs => \@opts }, $class;
+   return bless {
+      specs => [ grep { ref $_ } @opts ],
+      notes => [],
+      instr => [ grep { !ref $_ } @opts ],
+      mutex => \@mutex,
+   }, $class;
 }
 
 # Gets options from @ARGV.
@@ -55,7 +90,36 @@ sub parse {
    Getopt::Long::Configure('no_ignore_case', 'bundling');
    GetOptions( map { $_->{s} => \$defaults{$_->{k}} } @specs )
       or $defaults{help} = 1;
+
+   if ( $defaults{version} ) {
+      (my $prog) = $PROGRAM_NAME =~ m/(mysql-[a-z-]+)$/;
+      printf("%s  Ver %s Distrib %s Changeset %s\n",
+         $prog, $main::VERSION, $main::DISTRIB, $main::SVN_REV);
+      exit(0);
+   }
+
+   # Check required options (oxymoron?)
+   foreach my $spec ( grep { $_->{r} } @specs ) {
+      if ( !defined $defaults{$spec->{k}} ) {
+         $defaults{help} = 1;
+         $self->note("Required option --$spec->{l} must be specified");
+      }
+   }
+
+   # Check mutex options
+   foreach my $mutex ( @{$self->{mutex}} ) {
+      my @set = grep { defined $defaults{$_} } @$mutex;
+      if ( @set > 1 ) {
+         $defaults{help} = 1;
+      }
+   }
+
    return %defaults;
+}
+
+sub note {
+   my ( $self, $note ) = @_;
+   push @{$self->{notes}}, $note;
 }
 
 # Prints out command-line help.  The format is like this:
@@ -96,6 +160,9 @@ sub usage {
       else {
          $usage .= sprintf("  --%-${lcol}s  %s\n", $long, $desc);
       }
+   }
+   if ( (my @notes = @{$self->{notes}}) ) {
+      $usage .= join("\n", 'Errors while processing:', @notes) . "\n";
    }
    return $usage;
 }
