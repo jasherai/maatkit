98e0d8506d3b977105dfc6c985a98e7b23f240a7
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-11-06 00:09:13 +0000

    keywords
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@1213 dfb901c2-3250-0410-b216-0b33211c9131

--- common/MySQLFind.pm
+++ common/MySQLFind.pm
@@ -15,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA.
 # ###########################################################################
-# MySQLFind package $Revision: 1178 $
+# MySQLFind package $Revision$
 # ###########################################################################
 use strict;
 use warnings FATAL => 'all';
