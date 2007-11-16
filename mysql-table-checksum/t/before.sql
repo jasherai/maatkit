f595f05034c5e0f28f9972ac98cddbb54cde9b78
bps7j <bps7j>
2007-11-16 14:35:15 +0000

    clean up test suite

--- mysql-table-checksum/t/before.sql
+++ mysql-table-checksum/t/before.sql
@@ -1,108 +1,22 @@
 use test;
 
-drop table if exists checksum_test;
-drop table if exists checksum_test_2;
-drop table if exists checksum_test_3;
-drop table if exists checksum_test_4;
-drop table if exists checksum_test_5;
+drop table if exists checksum_test, checksum;
 
 create table checksum_test(
-   a int not null primary key,
-   b int,
-   c int not null
-) type=innodb;
+   a int not null primary key
+);
 
-insert into checksum_test(a, b, c) values
-   (1, 2, 3), (2, NULL, 3);
+insert into checksum_test(a) values (1);
 
-create table checksum_test_2(
-   a int not null primary key,
-   b int,
-   c int not null
-) type=innodb;
-
-insert into checksum_test_2 select * from checksum_test;
-
-create table checksum_test_3(
-   a int not null primary key,
-   b int,
-   c int not null
-) type=innodb;
-
-insert into checksum_test_3 values
-   (1,3,8),
-   (3,238,1),
-   (2,38,147);
-
-create table checksum_test_4(
-   a int,
-   b int,
-   c int not null,
-   unique key(a)
-) type=innodb;
-
-insert into checksum_test_4 values
-   (NULL,3,8),
-   (3,3,8);
-
-create table checksum_test_5(
-   a date not null primary key,
-   b int
-) type=innodb;
-
-insert into checksum_test_5 values
-   ('2000-01-01', 5),
-   ('2001-01-01', 5);
-
-drop table if exists checksum_test_6;
-create table checksum_test_6(
-   a datetime not null primary key,
-   b int
-) type=innodb;
-insert into checksum_test_6 values
-   ('1922-01-14 05:18:23', 5),
-   ('1950-03-21 09:03:15', 88),
-   ('2005-11:26 00:59:19', 234);
-
-drop table if exists checksum_test_7;
-create table checksum_test_7(
-   a time not null primary key,
-   b int
-) type=innodb;
-insert into checksum_test_7 values
-   ('05:18:23', 5),
-   ('09:03:15', 88),
-   ('00:59:19', 234);
-
-drop table if exists checksum;
-
-  CREATE TABLE checksum (
-     db         char(64)     NOT NULL,
-     tbl        char(64)     NOT NULL,
-     chunk      int          NOT NULL,
-     boundaries char(64)     NOT NULL,
-     this_crc   char(40)     NOT NULL,
-     this_cnt   int          NOT NULL,
-     master_crc char(40)         NULL,
-     master_cnt int              NULL,
-     ts         timestamp    NOT NULL,
-     PRIMARY KEY (db, tbl, chunk)
-  );
-
-drop table if exists checksum_test_8;
-create table checksum_test_8 (
-   a double not null primary key
-)type=myisam;
-insert into checksum_test_8 values(1), (3), (99.999);
-
-drop table if exists checksum_test_9;
-create table checksum_test_9 (
-   a float not null primary key
-)type=myisam;
-insert into checksum_test_9 values(1), (3), (99.999);
-
-drop table if exists checksum_test_10;
-create table checksum_test_10 (
-   a decimal not null primary key
-)type=myisam;
-insert into checksum_test_10 values(1), (3), (99.999);
+CREATE TABLE checksum (
+ db         char(64)     NOT NULL,
+ tbl        char(64)     NOT NULL,
+ chunk      int          NOT NULL,
+ boundaries char(64)     NOT NULL,
+ this_crc   char(40)     NOT NULL,
+ this_cnt   int          NOT NULL,
+ master_crc char(40)         NULL,
+ master_cnt int              NULL,
+ ts         timestamp    NOT NULL,
+ PRIMARY KEY (db, tbl, chunk)
+);
