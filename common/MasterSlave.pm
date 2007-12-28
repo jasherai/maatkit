89416acf551de970e6b1fb72f121028562b946aa
bps7j <bps7j@dfb901c2-3250-0410-b216-0b33211c9131>
2007-12-28 19:39:26 +0000

    factor out code for recursing to slaves
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@1605 dfb901c2-3250-0410-b216-0b33211c9131

---
+++ common/MasterSlave.pm
@@ -0,0 +1,115 @@
+# This program is copyright (c) 2007 Baron Schwartz.
+# Feedback and improvements are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2; OR the Perl Artistic License.  On UNIX and similar
+# systems, you can issue `man perlgpl' or `man perlartistic' to read these
+# licenses.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+# ###########################################################################
+# MasterSlave package $Revision$
+# ###########################################################################
+use strict;
+use warnings FATAL => 'all';
+
+package MasterSlave;
+
+use English qw(-no_match_vars);
+use List::Util qw(min max);
+
+sub new {
+   bless {}, shift;
+}
+
+# Descends to slaves by examining SHOW SLAVE HOSTS.  Arguments is a hashref:
+#
+# * dbh           (Optional) a DBH.
+# * dsn           The DSN to connect to; if no DBH, will connect using this.
+# * dsn_parser    A DSNParser object.
+# * recurse       How many levels to recurse. 0 = none, undef = infinite.
+# * callback      Code to execute after finding a new slave.
+#
+# The callback gets the slave's DSN, dbh, and the recursion level as args.
+# The recursion is tail recursion.
+sub recurse_to_slaves {
+   my ( $self, $args, $level ) = @_;
+   $level ||= 0;
+   my $dp   = $args->{dsn_parser};
+   my $dsn  = $args->{dsn};
+
+   my $dbh;
+   eval {
+      $dbh = $args->{dbh} || $dp->get_dbh(
+         $dp->get_cxn_params($dsn), { AutoCommit => 1 });
+      $ENV{MKDEBUG} && _d('Connected to ', $dp->as_string($dsn));
+   };
+   if ( $EVAL_ERROR ) {
+      print STDERR "Cannot connect to ", $dp->as_string($dsn), "\n";
+      return;
+   }
+
+   # SHOW SLAVE HOSTS sometimes has obsolete information.  Verify that this
+   # server has the ID its master thought, and that we have not seen it before
+   # in any case.
+   my $sql  = 'SELECT @@SERVER_ID';
+   $ENV{MKDEBUG} && _d($sql);
+   my ($id) = $dbh->selectrow_array($sql);
+   $ENV{MKDEBUG} && _d('Working on server ID ', $id);
+   my $master_thinks_i_am = $dsn->{server_id};
+   if ( !defined $id
+       || ( defined $master_thinks_i_am && $master_thinks_i_am != $id )
+       || $args->{server_ids_seen}->{$id}++
+   ) {
+      $ENV{MKDEBUG} && _d('Server ID seen, or not what master said');
+      print STDERR "Skipping ", $dp->as_string($dsn), "\n";
+      return;
+   }
+
+   # Call the callback!
+   $args->{callback}->($dsn, $dbh, $level);
+
+   if ( !defined $args->{recurse} || $level < $args->{recurse} ) {
+
+      # Find the slave hosts.  Eliminate hosts that aren't slaves of me (as
+      # revealed by server_id and master_id).  SHOW SLAVE HOSTS can be wacky.
+      my @slaves =
+         grep { $_->{master_id} == $id } # Only my own slaves.
+         map  {                          # Convert each to all-lowercase keys.
+            my %hash;
+            @hash{ map { lc $_ } keys %$_ } = values %$_;
+            \%hash;
+         }
+         @{$dbh->selectall_arrayref("SHOW SLAVE HOSTS", { Slice => {} })};
+
+      foreach my $slave ( @slaves ) {
+         my $spec = "h=$slave->{host},P=$slave->{port}"
+            . ( $slave->{user} ? ",u=$slave->{user}" : '')
+            . ( $slave->{password} ? ",p=$slave->{password}" : '');
+         my $dsn = $dp->parse($spec, $dsn);
+         $dsn->{server_id} = $slave->{server_id};
+         $ENV{MKDEBUG} && _d('Recursing from ',
+            $dp->as_string($dsn), ' to ', $dp->as_string($dsn));
+         $self->recurse_to_slaves(
+            { %$args, dsn => $dsn, dbh => undef }, $level + 1 );
+      }
+   }
+}
+
+sub _d {
+   my ( $line ) = (caller(0))[2];
+   print "# MasterSlave:$line ", @_, "\n";
+}
+
+1;
+
+# ###########################################################################
+# End MasterSlave package
+# ###########################################################################
---
+++ common/t/MasterSlave.t
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+# This program is copyright (c) 2007 Baron Schwartz.
+# Feedback and improvements are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2; OR the Perl Artistic License.  On UNIX and similar
+# systems, you can issue `man perlgpl' or `man perlartistic' to read these
+# licenses.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 2;
+use English qw(-no_match_vars);
+
+require "../MasterSlave.pm";
+require "../DSNParser.pm";
+
+my $dp = new DSNParser();
+
+sub throws_ok {
+   my ( $code, $pat, $msg ) = @_;
+   eval { $code->(); };
+   like ( $EVAL_ERROR, $pat, $msg );
+}
+
+# First, see whether a sandbox server is running.
+my $proc = `ps -eaf | grep rsandbox | grep master | grep port`;
+
+# The output might look like this:
+# baron    14396 14363  0 14:08 pts/1    00:00:00
+# /home/baron/mysql_source/5.0.45/bin/mysqld
+# --defaults-file=/home/baron/rsandbox_5_0_45/master/my.sandbox.cnf
+# --basedir=/home/baron/mysql_source/5.0.45
+# --datadir=/home/baron/rsandbox_5_0_45/master/data
+# --pid-file=/home/baron/rsandbox_5_0_45/master/data/mysql_sandbox16045.pid
+# --skip-external-locking --port=16045 --socket=/tmp/mysql_sandbox16045.sock
+
+my ($port) = $proc =~ m/--port=(\d+)/;
+
+SKIP: {
+   skip 2, 'No sandbox server running' unless $port;
+
+   # Connect
+   my $dsn = $dp->parse("h=127.0.0.1,P=$port");
+   my $dbh = $dp->get_dbh($dp->get_cxn_params($dsn), { AutoCommit => 1 });
+
+   my $ms = new MasterSlave();
+   my $callback = sub {
+      my ( $dsn, $dbh, $level ) = @_;
+      return unless $level;
+      ok($dsn, "Connected to one slave " . ($dp->as_string($dsn) || '<none>'));
+   };
+   $ms->recurse_to_slaves(
+      { dsn_parser => $dp, dbh => $dbh, dsn => $dsn, recurse => 1, callback => $callback });
+}
