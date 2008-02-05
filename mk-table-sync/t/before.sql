3a5bec2bc95015fbc8cf84df992f844c44f22f96
bps7j <bps7j>
2008-02-05 15:51:23 +0000

    Numeric strings beginning with 0 weren't quoted (bug #1883019).

--- mk-table-sync/t/before.sql
+++ mk-table-sync/t/before.sql
@@ -11,3 +11,12 @@ create table test1(
 
 create table test2 like test1;
 insert into test1 values(1, 'en'), (2, 'ca');
+
+drop table if exists test3, test4;
+create table test3 (
+  id int not null primary key,
+  name varchar(255)
+);
+insert into test3(id, name) values(15034, '51707'),(1, '001');
+create table test4 like test3;
+insert into test4(id, name) values(15034, '051707'),(1, '1');
