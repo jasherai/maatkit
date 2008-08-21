d699b31cc05cd887ffe933cef2cdb148072dad3d
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2008-08-21 22:33:12 +0000

    propset svn:keywords Revision Daemon.pm
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@2223 dfb901c2-3250-0410-b216-0b33211c9131

--- common/Daemon.pm
+++ common/Daemon.pm
@@ -15,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA.
 # ###########################################################################
-# Daemon package $Revision: $
+# Daemon package $Revision$
 # ###########################################################################
 
 # Daemon - Daemonize and handle daemon-related tasks
