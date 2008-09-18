6c4808e6aeac0e62197ff31bae791f68766b64a4
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2008-09-18 04:08:58 +0000

    Adding LogSplitter
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@2313 dfb901c2-3250-0410-b216-0b33211c9131

---
+++ common/LogSplitter.pm
@@ -0,0 +1,96 @@
+# This program is copyright 2008 Percona Inc.
+# Feedback and improvements are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2; OR the Perl Artistic License.  On UNIX and similar
+# systems, you can issue `man perlgpl' or `man perlartistic' to read these
+# licenses.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+
+# ###########################################################################
+# LogSplitter package $Revision$
+# ###########################################################################
+
+package LogSplitter;
+
+use strict;
+use warnings FATAL => 'all';
+use English qw(-no_match_vars);
+
+use constant MKDEBUG => $ENV{MKDEBUG};
+
+sub new {
+   my ( $class ) = @_;
+   bless {}, $class;
+}
+
+sub split_logs {
+   my ( $self, %args ) = @_;
+   foreach my $arg ( qw(log_files attribute saveto_dir LogParser) ) {
+      die "I need a $arg argument" unless $args{$arg};
+   }
+
+   my @fhs;
+   foreach my $log ( @{ $args{log_files} } ) {
+      open my $fh, "<", $log or die "Cannot open $log: $OS_ERROR\n";
+      push @fhs, $fh;
+   }
+
+   $self->{attribute}  = $args{attribute};
+   $self->{saveto_dir} = $args{saveto_dir};
+   $self->{sessions}   = ();
+   $self->{n_sessions} = 0;
+
+   my $callback = sub {
+      my ( $event ) = @_;
+
+      my $attrib = $self->{attribute};
+      if ( !exists $event->{ $attrib } ) {
+         die "Attribute $attrib does not exist in log events.";
+      }
+
+      my $log_split_fh;
+      my $session = $event->{ $attrib };
+      if ( exists $self->{sessions}->{ $session } ) { # Existing session
+         $log_split_fh = $self->{sessions}->{ $session };
+      }
+      else { # New session
+         my $session_n = sprintf '%04d', ++$self->{n_sessions};
+         my $log_split_file = $self->{saveto_dir}
+                            . "mysql_log_split-$session_n";
+
+         open $log_split_fh, ">", $log_split_file
+            or die "Cannot open log split file $log_split_file: $OS_ERROR";
+         $self->{sessions}->{ $session } = $log_split_fh;
+         MKDEBUG && _d("Created $log_split_file for session $attrib=$session");
+      }
+
+      print $log_split_fh "$event->{arg}\n\n";
+   };
+
+   my $lp = $args{LogParser};
+   foreach my $fh ( @fhs ) {
+      1 while $lp->parse_event($fh, $callback)
+   }
+
+   return;
+}
+
+sub _d {
+   my ( $line ) = (caller(0))[2];
+   print "# LogSplitter:$line $PID ", @_, "\n";
+}
+
+1;
+
+# ###########################################################################
+# End LogSplitter package
+# ###########################################################################
---
+++ common/t/LogSplitter.t
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 8;
+use English qw(-no_match_vars);
+
+require '../LogSplitter.pm';
+require '../LogParser.pm';
+
+my $lp = new LogParser;
+my $ls = new LogSplitter;
+
+isa_ok($ls, 'LogSplitter');
+
+my $tmpdir = '/tmp/logettes';
+diag(`mkdir $tmpdir`);
+
+eval {
+   $ls->split_logs(
+      log_files  => [ 'samples/slow006.txt' ],
+      attribute  => 'foo',
+      saveto_dir => "$tmpdir/",
+      LogParser  => $lp,
+   );
+};
+like($EVAL_ERROR, qr/Attribute foo does not exist in log events/, 'Dies if attribute does not exist');
+
+
+$ls->split_logs(
+   log_files  => [ 'samples/slow006.txt' ],
+   attribute  => 'Thread_id',
+   saveto_dir => "$tmpdir/",
+   LogParser  => $lp,
+);
+
+ok(-f "$tmpdir/mysql_log_split-0001", 'Basic log split 0001 exists');
+ok(-f "$tmpdir/mysql_log_split-0002", 'Basic log split 0002 exists');
+ok(-f "$tmpdir/mysql_log_split-0003", 'Basic log split 0003 exists');
+
+my $output;
+
+$output = `diff $tmpdir/mysql_log_split-0001 samples/slow006_split-0001.txt`;
+ok(!$output, 'Basic log split 0001 has correct SQL statements');
+$output = `diff $tmpdir/mysql_log_split-0002 samples/slow006_split-0002.txt`;
+ok(!$output, 'Basic log split 0002 has correct SQL statements');
+$output = `diff $tmpdir/mysql_log_split-0003 samples/slow006_split-0003.txt`;
+ok(!$output, 'Basic log split 0003 has correct SQL statements');
+
+diag(`rm -rf $tmpdir`);
+exit;
