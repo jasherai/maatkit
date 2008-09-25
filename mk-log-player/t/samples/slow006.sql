c30f863fba0baadc939a79fd15832e2367f40f35
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2008-09-25 04:19:58 +0000

    Update mk-log-player
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@2350 dfb901c2-3250-0410-b216-0b33211c9131

--- mk-log-player/t/samples/slow006.sql
+++ mk-log-player/t/samples/slow006.sql
@@ -1,16 +1,18 @@
 -- This is meant to be used with ../common/t/samples/slow006.txt
 -- and the log splits which result from it.
 
-CREATE DATABASE foo;
-USE foo;
+DROP DATABASE IF EXISTS mklp_foo;
+CREATE DATABASE mklp_foo;
+USE mklp_foo;
 CREATE TABLE foo_tbl (
    col INT
 );
-INSERT INTO foo_tbl VALUES (1),(3),(5),(7),(9);
+INSERT INTO foo_tbl VALUES (1),(3),(5),(7),(9),(11),(13),(15),(17),(19),(21);
 
-CREATE DATABASE bar;
-USE bar;
+DROP DATABASE IF EXISTS mklp_bar;
+CREATE DATABASE mklp_bar;
+USE mklp_bar;
 CREATE TABLE bar_tbl (
    col INT
 );
-INSERT INTO bar_tbl VALUES (2),(4),(6),(8),(10);
+INSERT INTO bar_tbl VALUES (2),(4),(6),(8),(10),(12),(14),(16),(18),(20);
