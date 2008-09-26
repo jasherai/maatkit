a65cd3b3f5bbe71a91c0966b08054fd58c3685c1
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2008-09-26 20:14:42 +0000

    Work on SQLMetrics
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@2352 dfb901c2-3250-0410-b216-0b33211c9131

--- common/SQLMetrics.pm
+++ common/SQLMetrics.pm
@@ -32,7 +32,14 @@ my %metric_type_for = (
    'number' => METRIC_TYPE_NUMERIC,
    'string' => METRIC_TYPE_STRING,
 );
+
 # TODO: 'time' metric type?
+# TODO: metric operations can be abstracted like:
+# %metric_operation_for = (
+#    min => sub { ... }
+#    max => sub { ... }
+#    ...
+# )
 
 use Data::Dumper;
 $Data::Dumper::Indent = 1;
@@ -41,16 +48,34 @@ $Data::Dumper::Indent = 1;
 # a hash for the handlers arg to new(). Example:
 # my $handlers = {
 #    make_handler_for('Query_time', 'number', ...),
-#    make_handler_for('User',       'string', ...),
+#    make_handler_for('user',       'string', ...),
 # }
 # Then:
 # $sm = new SQLMetrics(
 #   key_metric      => 'arg',
+#   fingerprint     => \&QueryRerwriter::fingerprint,
 #   handlers        => $handlers,
 #   buffer_n_events => 1_000,
-#   ...
 # );
-# TODO: document this further
+# Optional args:
+#    transform    : sub ref called and passed the metric value before any
+#                   calculations
+#                   (default none)
+#    total        : boolean, if total value should be saved (e.g. total
+#                   Query_time for each unique query)
+#                   (default 1)
+#    min, max, avg: boolean, if min max and/or avg values should be saved
+#                   (default 1 1 1)
+#    all_vals     : boolean, save all metric vals for each enabled
+#                   query-specific metric
+#                   (default 1) 
+#    all_events   : boolean, for any of the query-specific metrics above which
+#                   are enabled, save also the metric for all queries
+#                   (e.g. grand total Query_time for all queries)
+#                   (default 0)
+#    all_all_vals : boolean, like all_vals but saves all vals for all enabled
+#                   query metrics, which can result in very large arrays of vals
+#                   (default 0)
 sub make_handler_for {
    my ( $metric, $type, %args ) = @_;
    die "I need a metric"      if !$metric;
@@ -59,8 +84,11 @@ sub make_handler_for {
    $type = $metric_type_for{$type} || die 'Invalid metric type';
 
    my %default_handler = (
-      type      => $type,
-      transform => undef,
+      type         => $type,
+      transform    => undef,
+      all_vals     => 1,
+      all_events   => 0,
+      all_all_vals => 0,
    );
    @default_handler{ qw(total min max avg) } = qw(1 1 1 1)
       if $type == METRIC_TYPE_NUMERIC;
@@ -89,24 +117,13 @@ sub new {
       die "I need a $arg argument" unless $args{$arg};
    }
 
-   # Default, skeleton metrics data structure.
-   my $metrics = {
-      all => {             # metrics for all events
-         'count' => 0,
-      },
-      'unique' => {        # metrics for unique events (fingerprints)
-      },
-   };
-
    bless {
-      metrics            => $metrics,
       key_metric         => $args{key_metric},
       fingerprint        => $args{fingerprint},
       handlers           => $args{handlers},
       buffer_n_events    => $args{buffer_n_events},
-      n_events           => 0, # total number of events
-      n_events_in_buffer => 0, # number of events currently in buffer
-      n_event_buffers    => 0, # number of event buffers filled/calced
+      metrics            => { all => {}, unique => {} },
+      n_events           => 0,
    }, $class;
 }
 
@@ -116,18 +133,20 @@ sub record_event {
    return if !$event;
 
    $self->{n_events}++;
-   $self->{n_events_in_buffer}++;
    push @buffered_events, $event;
    MKDEBUG && _d("$self->{n_events} events total, "
-                 . "$self->{n_events_in_buffer} events in buffer");
+                 . scalar @buffered_events . " events in buffer");
 
    # Return if we are to buffer every event.
    return if $self->{buffer_n_events} < 0;
 
    # Return if we are to buffer N events and buffer space remains.
-   return if $self->{n_events_in_buffer} < $self->{buffer_n_events};
+   return if scalar @buffered_events < $self->{buffer_n_events};
 
    $self->calc_metrics();
+
+   # Reset buffer if it is full.
+   $self->reset_buffer() if scalar @buffered_events >= $self->{buffer_n_events};
 
    return;
 }
@@ -167,47 +186,69 @@ sub calc_metrics {
          # Get shortcut to this metric's handlers.
          my $handler = $self->{handlers}->{ $metric };
 
-         # Get shortcut to data store for this metric, for this fingerprint.
+         # Get shortcut to data store for this metric, for this event
+         # and for all events
          my $m_ds = $fp_ds->{ $metric } ||= { };
+         my $a_ds = $self->{metrics}->{all}->{ $metric } ||= { };
 
-         # ########################## #
-         # Finally, calc this metric. #
-         # ########################## #
+         # ################# #
+         # Calc this metric. #
+         # ################# #
          $metric_val = $handler->{transform}->($metric_val)
             if defined $handler->{transform};
 
          if ( $handler->{type} == METRIC_TYPE_NUMERIC ) {
-            push @{ $m_ds->{all_vals} }, $metric_val;
+            push @{ $m_ds->{all_vals} }, $metric_val
+               if $handler->{all_vals};
+            push @{ $a_ds->{all_vals} }, $metric_val
+               if $handler->{all_all_vals};
 
-            $m_ds->{total} += $metric_val if $handler->{total};
-            
+            if ( $handler->{total} ) {
+               $m_ds->{total} += $metric_val;
+               $a_ds->{total} += $metric_val if $handler->{all_events};
+            }
+
             if ( $handler->{min} ) {
                $m_ds->{min} = 0 if !defined $m_ds->{min};
                $m_ds->{min} = $metric_val if $metric_val < $m_ds->{min};
+
+               if ( $handler->{all_events} ) {
+                  $a_ds->{min} = 0 if !defined $a_ds->{min};
+                  $a_ds->{min} = $metric_val if $metric_val < $a_ds->{min};
+               }
             }
             if ( $handler->{max} ) {
-               $m_ds->{max} = 0 if !defined $m_ds->{min};
-               $m_ds->{max} = $metric_val if $metric_val > $m_ds->{min};
+               $m_ds->{max} = 0 if !defined $m_ds->{max};
+               $m_ds->{max} = $metric_val if $metric_val > $m_ds->{max};
+
+               if ( $handler->{all_events} ) {
+                  $a_ds->{max} = 0 if !defined $a_ds->{max};
+                  $a_ds->{max} = $metric_val if $metric_val > $a_ds->{max};
+               }
             }
             if ( $handler->{avg} ) {
                my $avg = $m_ds->{total} / $fp_ds->{count};
-               $m_ds->{avg} = defined $handler->{transform}
-                                 ? $handler->{transform}->($avg)
-                                 : $avg;
+               $avg = $handler->{transform}->($avg)
+                  if defined $handler->{transform};
+               $m_ds->{avg} = $avg;
+
+               if ( $handler->{all_events} ) {
+                  $avg = $a_ds->{total} / $self->{n_events};
+                  $avg = $handler->{transform}->($avg)
+                     if defined $handler->{transform};
+                  $a_ds->{avg} = $avg;
+               }
             }
          }
          elsif ( $handler->{type} == METRIC_TYPE_STRING ) {
             # Save and count unique occurrences of strings.
             $m_ds->{ $metric_val }++;
+
+            $a_ds->{ $metric_val }++ if $handler->{all_events};
          }
       }
    }
 
-   # Reset buffer if it is full.
-   if ( $self->{buffer_n_events} >= $self->{n_events_in_buffer} ) {
-      @buffered_events = ();
-      MKDEBUG && _d('Reset event buffer');
-   }
    return;
 }
 
@@ -230,6 +271,22 @@ sub nth_percent {
    @s = splice(@s, 0, $n); # remove vals after cut-off percent
 
    return \@s;
+}
+
+sub reset_buffer {
+   my ( $self ) = @_;
+   @buffered_events = ();
+   MKDEBUG && _d('Reset event buffer');
+   return;
+}
+
+sub reset_metrics {
+   my ( $self ) = @_;
+   @buffered_events           = ();
+   $self->{n_events}          = 0;
+   $self->{metrics}->{all}    = {};
+   $self->{metrics}->{unique} = {};
+   return;
 }
 
 sub _d {
--- common/t/SQLMetrics.t
+++ common/t/SQLMetrics.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 use English qw(-no_match_vars);
 
 require '../QueryRewriter.pm';
@@ -13,8 +13,8 @@ use Data::Dumper;
 $Data::Dumper::Indent = 1;
 
 my $handlers = {
-   SQLMetrics::make_handler_for('Query_time', 'number'),
-   SQLMetrics::make_handler_for('user', 'string'),
+   SQLMetrics::make_handler_for('Query_time', 'number', (all_events=>1)),
+   SQLMetrics::make_handler_for('user', 'string', (all_events=>1)),
 };
 
 my $m  = new SQLMetrics(
@@ -32,7 +32,7 @@ my $events = [
       user          => 'root',
       host          => 'localhost',
       ip            => '',
-      arg           => "SELECT id FROM users WHERE name='baouong'",
+      arg           => "SELECT id FROM users WHERE name='foo'",
       Query_time    => '0.000652',
       Lock_time     => '0.000109',
       Rows_sent     => 1,
@@ -51,13 +51,82 @@ my $events = [
       Rows_examined => 0,
       NR            => 8,
    },
+   {
+      cmd           => 'Query',
+      user          => 'bob',
+      host          => 'localhost',
+      ip            => '',
+      arg           => "SELECT id FROM users WHERE name='bar'",
+      Query_time    => '0.000682',
+      Lock_time     => '0.000201',
+      Rows_sent     => 1,
+      Rows_examined => 2,
+      NR            => 11,
+   }
 ];
+
+my $metrics = {
+   'unique' => {
+      'insert ignore into articles (id, body,)values(N+)' => {
+         'count' => 1,
+         'Query_time' => {
+            'avg' => '0.001943',
+            'min' => 0,
+            'max' => '0.001943',
+            'all_vals' => [
+            '0.001943'
+            ],
+            'total' => '0.001943'
+         },
+         'user' => {
+            'root' => 1
+         },
+         'sample' => 'INSERT IGNORE INTO articles (id, body,)VALUES(3558268,\'sample text\')'
+      },
+      'select id from users where name=S' => {
+         'count' => 2,
+         'Query_time' => {
+            'avg' => '0.000667',
+            'min' => 0,
+            'max' => '0.000682',
+            'all_vals' => [
+               '0.000652',
+               '0.000682'
+            ],
+            'total' => '0.001334'
+         },
+         'user' => {
+            'bob' => 1,
+            'root' => 1
+         },
+         'sample' => 'SELECT id FROM users WHERE name=\'foo\''
+      }
+   },
+   all => {
+      'Query_time' => {
+         'avg' => '0.00109233333333333',
+         'min' => 0,
+         'max' => '0.001943',
+         'total' => '0.003277',
+      },
+      'user' => {
+         'bob'  => 1,
+         'root' => 2,
+      },
+   },
+};
 
 foreach my $event ( @$events ) {
    $m->record_event($event);
 }
 $m->calc_metrics();
+is_deeply($m->{metrics}, $metrics, 'Calcs buffered metrics');
 
-print Dumper($m);
+$m->reset_metrics();
+$m->{buffer_n_events} = 1;
+foreach my $event ( @$events ) {
+   $m->record_event($event);
+}
+is_deeply($m->{metrics}, $metrics, 'Calcs metrics one-by-one');
 
 exit;
