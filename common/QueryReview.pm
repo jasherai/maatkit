f24e620022167fa2b007d84e19a5321aeece66d6
baron.schwartz <baron.schwartz@dfb901c2-3250-0410-b216-0b33211c9131>
2009-03-01 22:32:04 +0000

    Greatly simplify QueryReview.pm in preparation for building better --review into mk-query-digest
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@3109 dfb901c2-3250-0410-b216-0b33211c9131

--- common/QueryReview.pm
+++ common/QueryReview.pm
@@ -21,32 +21,8 @@
 package QueryReview;
 
 # This module is an interface to a "query review table" in which certain
-# historical information about unique queries is stored. This review
-# information is primarily used by mk-log-parser to do a "query review"
-# which allows you to keep track of old/known queries running on the
-# server and easily discover when new queries appear on the server.
-#
-# The main work of this module is updating the query review table.
-# The minimal review table has the following columns:
-#    first_seen
-#    last_seen
-#    reviewed_by
-#    reviewed_on
-#    comments
-# The QueryReview module keeps these (and potentially other) columns
-# updated for each unique query (also called "events"). The module is
-# given events by calling cache_event() which adds the event to the
-# review table if it's new, then updates a local cache of the event's
-# review information (last_seen, etc.). A cached is used to avoid
-# thousands of little updates every second. Calling flush_event_cache()
-# causes the review table to be updated for all the events in the
-# cache which have changed since the last save.
-#
-# Events in the query review table are identified by a checksum. The
-# checksum is part of an MD5 hash of the query's group-by value (usually the
-# fingerprint of the $event->{arg}).
-
-# TODO: we need to use prepared statements for SQL in this class.
+# historical information about classes of queries is stored.  See the docs on
+# mk-query-digest for context.
 
 use strict;
 use warnings FATAL => 'all';
@@ -57,223 +33,102 @@ use Data::Dumper;
 
 use constant MKDEBUG => $ENV{MKDEBUG};
 
+# These columns are the minimal set of columns for every review table.  TODO:
+# maybe it's possible to specify this in the tool's POD and pass it in so it's
+# not hardcoded here and liable to get out of sync.
+my %basic_cols = map { $_ => 1 }
+   qw(checksum fingerprint sample first_seen last_seen reviewed_by
+      reviewed_on comments);
+my %skip_cols  = map { $_ => 1 } qw(fingerprint sample checksum);
+
 # Required args:
-# group_by      The name of the attribute in the event by which events will be
-#               grouped into a class.  See EventAggregator.pm for more on this.  The
-#               value is used to generate a GUID-ish value that we can use as
-#               the primary key in a database table.
 # dbh           A dbh to the server with the query review table.
-# db_tbl        Full db.tbl name of the query review table.
+# db_tbl        Full quoted db.tbl name of the query review table.
 #               Make sure the table exists! It's not checked here;
 #               check it before instantiating an object.
 # tbl_struct    Return val from TableParser::parse() for db_tbl.
 #               This is used to discover what columns db_tbl has.
+# quoter        Quoter object.
 #
 # Optional args:
-# where         SQL clause to limit pre-loaded fingerprints that
-#               comes after 'FROM db_tbl'.  It can be a WHERE clause
-#               and/or LIMIT.
 # ts_default    SQL expression to use when inserting a new row into
 #               the review table.  If nothing else is specified, NOW()
-#               is the default.
+#               is the default.  This is for dependency injection while
+#               testing.
 sub new {
    my ( $class, %args ) = @_;
-   foreach my $arg ( qw(dbh db_tbl tbl_struct group_by) ) {
+   foreach my $arg ( qw(dbh db_tbl tbl_struct quoter) ) {
       die "I need a $arg argument" unless $args{$arg};
    }
 
-   my @basic_cols
-      = qw(checksum fingerprint sample first_seen last_seen
-           reviewed_by reviewed_on comments);
-   foreach my $col ( @basic_cols ) {
+   foreach my $col ( keys %basic_cols ) {
       die "Query review table $args{db_tbl} does not have a $col column"
          unless $args{tbl_struct}->{is_col}->{$col};
    }
-   my %basic_cols = map { $_ => 1 } @basic_cols;
-   my @extra_cols = grep { !$basic_cols{$_} } @{$args{tbl_struct}->{cols}};
-
-   # Pre-load cache of fingerprint-checksums from the query review table.
-   my $sql = "SELECT fingerprint, CONV(checksum, 10, 16) as checksum_hex, "
-           . "first_seen, last_seen "
-           . "FROM $args{db_tbl} "
-           . ($args{where} ? $args{where} : '');
-   my %cache = map {
-      $_->{fingerprint} => {
-         checksum => $_->{checksum_hex},
-         dirty    => 0,
-         cols     => {
-            first_seen => $_->{first_seen},
-            last_seen  => $_->{last_seen},
-         }
-      }
-   }
-   @{ $args{dbh}->selectall_arrayref($sql, { Slice => {} }) };
 
    my $now = defined $args{ts_default} ? $args{ts_default} : 'NOW()';
-   $sql =
-         'INSERT IGNORE INTO ' . $args{db_tbl}
+
+   # Design statements to INSERT and statements to SELECT from the review table.
+   my $sql = 'INSERT INTO ' . $args{db_tbl}
          . '(checksum, fingerprint, sample, first_seen, last_seen) VALUES( '
-         . "CONV(?, 16, 10), ?, ?, COALESCE(?, $now), COALESCE(?, $now))";
-   MKDEBUG && _d("SQL to insert into review table:", $sql);
-   my $insert_new_sth = $args{dbh}->prepare($sql);
+         . "CONV(?, 16, 10), ?, ?, COALESCE(?, $now), COALESCE(?, $now)) "
+         . "ON DUPLICATE KEY UPDATE "
+         . "first_seen = LEAST(COALESCE(first_seen, $now), COALESCE(?, $now)), "
+         . "last_seen = GREATEST(COALESCE(last_seen, $now), COALESCE(?, $now))";
+   MKDEBUG && _d("SQL to insert into review table: ", $sql);
+   my $insert_sth = $args{dbh}->prepare($sql);
+
+   # The SELECT statement does not need to get the fingerprint, sample or
+   # checksum.
+   my @review_cols = grep { !$skip_cols{$_} } @{$args{tbl_struct}->{cols}};
+   $sql = "SELECT "
+        . join(', ', map { $args{quoter}->quote($_) } @review_cols)
+        . ", CONV(checksum, 10, 16) AS checksum_conv FROM $args{db_tbl}"
+        . " WHERE checksum=CONV(?, 16, 10)";
+   MKDEBUG && _d("SQL to select from review table: ", $sql);
+   my $select_sth = $args{dbh}->prepare($sql);
 
    my $self = {
-      dbh            => $args{dbh},
-      db_tbl         => $args{db_tbl},
-      cache          => \%cache,
-      insert_new_sth => $insert_new_sth,
-      group_by       => $args{group_by},
-      basic_cols     => \@basic_cols,
-      extra_cols     => \@extra_cols,
-      ts_default     => $now,
+      dbh         => $args{dbh},
+      db_tbl      => $args{db_tbl},
+      insert_sth  => $insert_sth,
+      select_sth  => $select_sth,
+      tbl_struct  => $args{tbl_struct},
    };
-   MKDEBUG && _d("new QueryReview obj: " . Dumper($self));
    return bless $self, $class;
 }
 
-my $review_sth;
-my @review_cols;
-
 # Fetch information from the database about a query that's been reviewed.
 sub get_review_info {
-   my ( $self, $q, $id, $db_tbl ) = @_;
-   if ( !$review_sth ) {
-      @review_cols = grep { $_ !~ m/^(?:fingerprint|sample|checksum)$/ }
-                     ( @{$self->{basic_cols}}, @{$self->{extra_cols}} );
-      my $sql = "SELECT "
-              . join(', ', map { $q->quote($_) } @review_cols)
-              . ", CONV(checksum, 10, 16) AS checksum_conv FROM "
-              . $q->quote($db_tbl->{D}, $db_tbl->{t})
-              . " WHERE checksum=CONV(?, 16, 10)";
-       MKDEBUG && _d("select for review vals: $sql");
-       $review_sth = $self->{dbh}->prepare($sql);
-   }
-   $review_sth->execute($id);
-   my $review_vals = $review_sth->fetchall_arrayref({});
+   my ( $self, $id ) = @_;
+   $self->{review_sth}->execute(make_checksum($id));
+   my $review_vals = $self->{review_sth}->fetchall_arrayref({});
    if ( $review_vals && @$review_vals == 1 ) {
-      $review_vals = $review_vals->[0];
-      delete $review_vals->{checksum};
-      return $review_vals;
+      return $review_vals->[0];
    }
+   return undef;
+}
+
+# Store a query into the table.  The arguments are:
+#  *  fingerprint
+#  *  sample
+#  *  first_seen
+#  *  last_seen
+# There's no need to convert the fingerprint to a checksum, no need to parse
+# timestamps either.
+sub set_review_info {
+   my ( $self, %args ) = @_;
+   $self->{insert_sth}->execute(
+      make_checksum($args{fingerprint}),
+      @args{qw(fingerprint sample)},
+      map { $args{$_} ? parse_timestamp($args{$_}) : undef }
+         qw(first_seen last_seen first_seen last_seen));
 }
 
 # Return the columns we'll be using from the review table.
 sub review_cols {
    my ( $self ) = @_;
-   if ( !@review_cols ) {
-      @review_cols = grep { $_ !~ m/^(?:fingerprint|sample|checksum)$/ }
-                     ( @{$self->{basic_cols}}, @{$self->{extra_cols}} );
-   }
-   return @review_cols;
-}
-
-sub cache_event {
-   my ( $self, $event ) = @_;
-   my $checksum;
-
-   # Skip events which do not have the group_by attribute.
-   my $group_by =  $event->{ $self->{group_by} };
-   return unless defined $group_by;
-
-   # Update the event in cache if it's an old event (either in cache or
-   # in the query review table). Else, add the new event to the query
-   # review table and the cache.
-   if ( exists $self->{cache}->{$group_by} ) {
-      my $fp_ds = $self->{cache}->{$group_by};
-      $checksum = $fp_ds->{checksum};
-      $fp_ds->{dirty} = 1;  # group_by in cache differs from query review tbl
-      # Update first_seen and last_seen. Timestamps may not always increase.
-      # They can decrease, for example, if the user parses an old log.
-      if ( $event->{ts} && ( my $ts = parse_timestamp($event->{ts}) ) ) {
-         my $cols = $fp_ds->{cols};
-         $cols->{first_seen}
-            = $ts if !$cols->{first_seen} || $ts le $cols->{first_seen};
-         $cols->{last_seen}
-            = $ts if !$cols->{last_seen}  || $ts ge $cols->{last_seen};
-      }
-   }
-   else {
-      $checksum = make_checksum($group_by);
-      if ( $self->event_is_stored($checksum) ) {
-         # Event not cached but stored in the db_tbl.
-         my $review_info = $self->{dbh}->selectall_hashref(
-            'SELECT CONV(checksum,10,16) AS checksum_conv, '
-            . join(', ', @{$self->{basic_cols}})
-            . ' FROM ' . $self->{db_tbl}
-            . " WHERE checksum=CONV('$checksum',16,10)",
-            'checksum_conv',);
-         $self->{cache}->{$group_by} = {
-            checksum => $checksum,
-            dirty    => 1,
-            cols     => {
-               first_seen => $review_info->{$checksum}->{first_seen} || '',
-               last_seen  => $review_info->{$checksum}->{last_seen}  || '',
-            },
-         };
-      }
-      else {
-         # New event.
-         # The primary key value (checksum column) is generated by checksumming
-         # the query and then converting part of the checksum into a bigint.
-         my $ts = $event->{ts} ? parse_timestamp($event->{ts}) : undef;
-
-         $self->{insert_new_sth}->execute(
-            $checksum,
-            $group_by,
-            $event->{arg},
-            $ts,
-            $ts);
-         MKDEBUG && _d("Stored new event: ", $checksum, $group_by, $ts);
-
-         $self->{cache}->{$group_by} = {
-            checksum => $checksum,
-            dirty    => 0,
-            cols     => {
-               first_seen => $ts,
-               last_seen  => $ts,
-            },
-         };
-      }
-   }
-
-   # Add event's checksum to itself as a pseudo-attribute.
-   $event->{checksum} = $checksum;
-}
-
-sub event_is_stored {
-   my ( $self, $checksum ) = @_;
-   return 1 if $self->{seen}->{$checksum};
-   my $sql = "SELECT checksum FROM $self->{db_tbl} "
-           . "WHERE checksum=CONV('$checksum',16,10)";
-   MKDEBUG && _d($sql);
-   my ($is_there) = $self->{dbh}->selectrow_array($sql);
-   $self->{seen}->{$checksum} = $is_there;
-   return scalar $is_there ? 1 : 0;
-}
-
-# Update query review table according to new values in cache.
-sub flush_event_cache {
-   my ( $self ) = @_;
-
-   CLASS:
-   foreach my $class ( keys %{$self->{cache}} ) {
-      my $fp_ds = $self->{cache}->{$class};
-      next CLASS unless $fp_ds->{dirty};
-      my $sql = "UPDATE $self->{db_tbl} SET "
-              . join(', ',
-                   map {
-                      # Special case for timestamp columns
-                      # TODO: column names need to be run through a Quoter!
-                      $_ =~ m/first_seen|last_seen/
-                         ? "$_=COALESCE(?, $self->{ts_default})"
-                         : "$_=?"
-                   } keys %{$fp_ds->{cols}}
-                )
-              . " WHERE checksum=CONV(?, 16, 10)";
-      MKDEBUG && _d("update sql for cached event: $sql");
-      my $sth = $self->{dbh}->prepare($sql);
-      $sth->execute(values %{$fp_ds->{cols}}, $fp_ds->{checksum});
-   }
+   return grep { !$skip_cols{$_} } @{$self->{tbl_struct}->{cols}};
 }
 
 sub _d {
--- common/t/QueryReview.t
+++ common/t/QueryReview.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings FATAL => 'all';
 
-use Test::More tests => 11;
+use Test::More tests => 3;
 use English qw(-no_match_vars);
 
 require '../DSNParser.pm';
@@ -31,56 +31,24 @@ my $du = new MySQLDump();
 my $tbl_struct = $tp->parse($du->dump($dbh, $q, 'test', 'query_review', 'table'));
 
 my $qv = new QueryReview(
-   group_by    => 'fingerprint',
    dbh        => $dbh,
-   db_tbl     => 'test.query_review',
+   db_tbl     => '`test`.`query_review`',
    tbl_struct => $tbl_struct,
    ts_default => '"2009-01-01"',
+   quoter     => $q,
 );
 
 isa_ok($qv, 'QueryReview');
-is_deeply(
-   $qv->{cache},
-   {
-      'select col from bar_tbl' => {
-         checksum => 'D4CD74934382A184',
-         dirty    => 0,
-         cols     => {
-            first_seen => '2005-12-19 16:56:31',
-            last_seen  => '2006-12-20 11:48:57',
-         }
-      },
-      'select col from foo_tbl' => {
-         checksum => 'A20C29AF174CE545',
-         dirty    => 0,
-         cols     => {
-            first_seen => '2007-12-18 11:48:27', 
-            last_seen  => '2007-12-18 11:48:27',
-         }
-      },
-   },
-   'Preloads fingerprints, checksums, first_seen and last_seen'
-);
-
-my @basic_cols = sort @{$qv->{basic_cols}};
-is_deeply(
-   \@basic_cols,
-   [qw(checksum comments fingerprint first_seen last_seen reviewed_by reviewed_on sample)],
-   'Has list of basic columns'
-);
-is_deeply(
-   $qv->{extra_cols},
-   [],
-   'Has no extra columns'
-);
-
-use Data::Dumper;
-$Data::Dumper::Indent=1;
 
 my $callback = sub {
    my ( $event ) = @_;
-   $event->{fingerprint} = $qr->fingerprint($event->{arg});
-   $qv->cache_event($event);
+   my $fp = $qr->fingerprint($event->{arg});
+   $qv->set_review_info(
+      fingerprint => $fp,
+      sample      => $event->{arg},
+      first_seen  => $event->{ts},
+      last_seen   => $event->{ts},
+   );
 };
 
 my $log;
@@ -90,7 +58,6 @@ close $log;
 open $log, '<', 'samples/slow021.txt' or bail_out($OS_ERROR);
 1 while ( $lp->parse_event($log, $callback) );
 close $log;
-$qv->flush_event_cache();
 
 my $res = $dbh->selectall_arrayref(
    'SELECT checksum, first_seen, last_seen FROM query_review order by checksum',
@@ -122,78 +89,33 @@ my $event = {
    ts  => '081222 13:13:13',
 };
 my $fp = $qr->fingerprint($event->{arg});
-$event->{fingerprint} = $fp;
 my $checksum = Transformers::make_checksum($fp);
-$qv->cache_event($event);
-is($event->{checksum}, $checksum, 'Adds checksum to event');
+$qv->set_review_info(
+   fingerprint => $fp,
+   sample      => $event->{arg},
+   first_seen  => $event->{ts},
+   last_seen   => $event->{ts},
+);
 
-$res = $dbh->selectall_arrayref("SELECT CONV(checksum,10,16), fingerprint,
-sample, first_seen, last_seen, reviewed_by, reviewed_on, comments
-FROM query_review
-WHERE checksum=CONV('$checksum',16,10)");
+$res = $dbh->selectall_arrayref(
+   "SELECT CONV(checksum,10,16) as checksum, fingerprint, sample, first_seen, "
+   . "last_seen, reviewed_by, reviewed_on, comments FROM test.query_review "
+   . "WHERE checksum=CONV('$checksum',16,10)",
+   {Slice => {}});
+
 is_deeply(
    $res,
-   [
-      [
-         $checksum,
-         $fp,
-         "UPDATE foo SET bar='nada' WHERE 1",
-         '2008-12-22 13:13:13',
-         '2008-12-22 13:13:13',
-         undef,undef,undef
-      ]
-   ],
+   [{
+      checksum    => $checksum,
+      fingerprint => $fp,
+      sample      => "UPDATE foo SET bar='nada' WHERE 1",
+      first_seen  => '2008-12-22 13:13:13',
+      last_seen   => '2008-12-22 13:13:13',
+      reviewed_by => undef,
+      reviewed_on => undef,
+      comments    => undef,
+   }],
    'Stores a new event with default values'
 );
-is($qv->{cache}->{$fp}->{checksum}, $checksum, 'Caches new checksum');
-
-# Test that it will query the table to see that the event is not new and
-# therefore update it instead of trying to add it again.
-$qv = new QueryReview(
-   group_by    => 'fingerprint',
-   dbh        => $dbh,
-   db_tbl     => 'test.query_review',
-   tbl_struct => $tbl_struct,
-   ts_default => '"2009-01-01"',
-);
-
-$event->{ts} = '081222 17:17:17';
-$qv->cache_event($event);
-$qv->flush_event_cache();
-$res = $dbh->selectall_arrayref(
-   "SELECT first_seen, last_seen FROM test.query_review "
-      . "WHERE checksum=CONV('$checksum',16,10)",
-   { Slice => {} });
-is_deeply(
-   $res,
-   [
-      {  first_seen => '2008-12-22 13:13:13',
-         last_seen  => '2008-12-22 17:17:17',
-      },
-   ],
-   'Updates old, non-cached event'
-);
-is($qv->{cache}->{$fp}->{checksum}, $checksum, 'Adds old event\'s checksum to cache');
-
-# Test that it will query the table to see that the event is not new and
-# therefore update it instead of trying to add it again.
-$dbh->do('truncate table test.query_review');
-$qv = new QueryReview(
-   group_by    => 'fingerprint',
-   dbh        => $dbh,
-   db_tbl     => 'test.query_review',
-   tbl_struct => $tbl_struct,
-   ts_default => '"2009-01-01"',
-);
-
-$event->{ts} = undef;
-eval {
-   $qv->cache_event($event);
-   $qv->cache_event($event);
-   $qv->flush_event_cache();
-   $qv->cache_event($event);
-   $qv->flush_event_cache();
-};
-is($EVAL_ERROR, '', 'No error on undef ts');
 
 $sb->wipe_clean($dbh);
