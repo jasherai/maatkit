871d7f9859b1ccefe5f5598a547d94a65005ab31
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2009-03-29 21:01:38 +0000

    Update KeySize.pm for issue 331.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@3290 dfb901c2-3250-0410-b216-0b33211c9131

--- common/KeySize.pm
+++ common/KeySize.pm
@@ -1,4 +1,4 @@
-# This program is copyright 2008-2009 Percona Inc.
+# This program is copyright 2008-@CURRENTYEAR@ Percona Inc.
 # Feedback and improvements are welcome.
 #
 # THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
@@ -31,20 +31,34 @@ sub new {
    return bless $self, $class;
 }
 
-# The tbl arg is an already quoted db-qualified table name, like `db`.`tbl`.
-# The key arg is a hashref to something like a value from the keys hash
-# in TableParser::parse(). It must contain at least a name and cols key.
-# The size of the key is return.
+# Returns the key's size in scalar context; returns the key's size
+# and the key that MySQL actually chose in list context.
+# Required args:
+#    name       => name of key
+#    cols       => arrayref of key's cols
+#    tbl_name   => quoted, db-qualified table name like `db`.`tbl`
+#    tbl_struct => hashref returned by TableParser::parse for tbl
+#    dbh        => dbh
+# If the key exists in the tbl (it should), then we can FORCE INDEX.
+# This is what we want to do because it's more reliable. But, if the
+# key does not exist in the tbl (which happens with foreign keys),
+# then we let MySQL choose the index.
 sub get_key_size {
    my ( $self, %args ) = @_;
-   foreach my $arg ( qw(tbl key dbh) ) {
+   foreach my $arg ( qw(name cols tbl_name tbl_struct dbh) ) {
       die "I need a $arg argument" unless $args{$arg};
    }
-   my $key = $args{key};
-   foreach my $req_key ( qw(name cols) ) {
-      die "The key hashref argument does not have a $req_key key"
-         unless exists $key->{$req_key};
+
+   my $name = $args{name};
+   my @cols = @{$args{cols}};
+
+   if ( @cols == 0 ) {
+      warn "No columns for key $name";
+      return 0;
    }
+  
+   my $key_exists = exists $args{tbl_struct}->{keys}->{ $name } ? 1 : 0;
+   MKDEBUG && _d('Key', $name, 'exists in', $args{tbl_name}, ':', $key_exists);
 
    # Construct a SQL statement with WHERE conditions on all key
    # cols that will get EXPLAIN to tell us 1) the full length of
@@ -54,12 +68,12 @@ sub get_key_size {
    # For 2), we have to break normal index usage which normally
    # allows MySQL to access only the limited number of rows needed
    # to satisify the query because we want to know total table rows.
-   my $sql = 'EXPLAIN SELECT ' . join(', ', @{$key->{cols}})
-           . ' FROM ' . $args{tbl}
-           . " FORCE INDEX (`$key->{name}`)"
+   my $sql = 'EXPLAIN SELECT ' . join(', ', @cols)
+           . ' FROM ' . $args{tbl_name}
+           . ($key_exists ? " FORCE INDEX (`$name`)" : '')
            . ' WHERE ';
    my @where_cols;
-   foreach my $col ( @{$key->{cols}} ) {
+   foreach my $col ( @cols ) {
       push @where_cols, "$col=1";
    }
    # For single column indexes we have to trick MySQL into scanning
@@ -67,28 +81,36 @@ sub get_key_size {
    # EXPLAIN rows will report only the rows that satisfy the query
    # using the key, but this is not what we want. We want total table rows.
    # In other words, we need an EXPLAIN type index, not ref or range.
-   if ( scalar @{$key->{cols}} == 1 ) {
-      push @where_cols, "$key->{cols}->[0]<>1";
+   if ( scalar @cols == 1 ) {
+      push @where_cols, "$cols[0]<>1";
    }
    $sql .= join(' OR ', @where_cols);
-   MKDEBUG && _d('Key size sql:', $sql);
+   MKDEBUG && _d('sql:', $sql);
 
-   my $explain = $args{dbh}->selectall_hashref($sql, 'id');
+   my $explain;
+   eval { $explain = $args{dbh}->selectall_hashref($sql, 'id'); };
+   if ( $args{dbh}->err ) {
+      warn "Cannot get size of $name key: $DBI::errstr";
+      return 0;
+   }
    my $key_len = $explain->{1}->{key_len};
    my $rows    = $explain->{1}->{rows};
+   my $key     = $explain->{1}->{key};
+
+   MKDEBUG && _d('MySQL chose key:', $key, 'len:', $key_len, 'rows:', $rows);
 
    my $key_size = 0;
    if ( defined $key_len && defined $rows ) {
       $key_size = $key_len * $rows;
    }
-   elsif ( MKDEBUG ) {
-      _d("key_len or rows NULL in EXPLAIN:\n",
+   else {
+      MKDEBUG && _d("key_len or rows NULL in EXPLAIN:\n",
          join("\n",
             map { "$_: ".($explain->{1}->{$_} ? $explain->{1}->{$_} : 'NULL') }
             keys %{$explain->{1}}));
    }
 
-   return $key_size;
+   return wantarray ? ($key_size, $key) : $key_size;
 }
 
 sub _d {
--- common/t/KeySize.t
+++ common/t/KeySize.t
@@ -1,10 +1,9 @@
 #!/usr/bin/perl
 
-
 use strict;
 use warnings FATAL => 'all';
-use Test::More tests => 4;
 use English qw(-no_match_vars);
+use Test::More tests => 6;
 
 require '../KeySize.pm';
 require '../TableParser.pm';
@@ -19,6 +18,14 @@ my $dp  = new DSNParser();
 my $sb  = new Sandbox(basedir => '/tmp', DSNParser => $dp);
 my $dbh = $sb->get_dbh_for('master')
    or BAIL_OUT('Cannot connect to sandbox master');
+my $q  = new Quoter();
+my $tp = new TableParser();
+my $ks = new KeySize();
+
+my $tbl;
+my $struct;
+my %key;
+my ($size, $chosen_key); 
 
 sub load_file {
    my ($file) = @_;
@@ -28,38 +35,60 @@ sub load_file {
    return $contents;
 }
 
-my $q  = new Quoter();
-my $tp = new TableParser();
-my $ks = new KeySize();
+sub key_info {
+   my ( $file, $db, $tbl, $key, $cols ) = @_;
+   $sb->load_file('master', $file, $db);
+   my $tbl_name = $q->quote($db, $tbl);
+   my $struct   = $tp->parse( load_file($file) );
+   return (
+      name       => $key,
+      cols       => $cols || $struct->{keys}->{$key}->{cols},
+      tbl_name   => $tbl_name,
+      tbl_struct => $struct,
+      dbh        => $dbh,
+   );
+}
+
+$sb->create_dbs($dbh, ['test']);
 
 isa_ok($ks, 'KeySize');
 
-$sb->create_dbs($dbh, ['test']);
-$sb->load_file('master', 'samples/dupe_key.sql', 'test');
-
-my $tbl    = $q->quote('test', 'dupe_key');
-my $struct = $tp->parse( load_file('samples/dupe_key.sql') );
-
-# The tbl is empty so MySQL should optimize away the query and
-# therefore key_len and rows will be NULL. This tests that
-# get_key_size doesn't blow up for such a case.
+# With an empty table, the WHERE is impossible, so MySQL should optimize
+# away the query, and key_len and rows will be NULL in EXPLAIN.
+%key = key_info('samples/dupe_key.sql', 'test', 'dupe_key', 'a');
 is(
-   $ks->get_key_size(tbl=>$tbl,key=>$struct->{keys}->{a},dbh=>$dbh),
+   $ks->get_key_size(%key),
    '0',
-   'dupe_key key a impossible where'
+   'empty table, impossible where'
 );
 
+# Populate the table to make the WHERE possible.
 $dbh->do('INSERT INTO test.dupe_key VALUE (1,2,3),(4,5,6),(7,8,9),(0,0,0)');
 is(
-   $ks->get_key_size(tbl=>$tbl,key=>$struct->{keys}->{a},dbh=>$dbh),
+   $ks->get_key_size(%key),
    '20',
-   'dupe_key key a'
+   'single column int key'
 );
 
+$key{name} = 'a_2';
 is(
-   $ks->get_key_size(tbl=>$tbl,key=>$struct->{keys}->{a_2},dbh=>$dbh),
+   $ks->get_key_size(%key),
    '40',
-   'dupe_key key a_2'
+   'two column int key'
+);
+
+$sb->load_file('master', 'samples/issue_331-parent.sql', 'test');
+%key = key_info('samples/issue_331.sql', 'test', 'issue_331_t2', 'fk_1', ['id']);
+($size, $chosen_key) = $ks->get_key_size(%key);
+is(
+   $size,
+   8,
+   'foreign key size'
+);
+is(
+   $chosen_key,
+   'PRIMARY',
+   'PRIMARY key chosen for foreign key'
 );
 
 $sb->wipe_clean($dbh);
