f4edad9e30cb5e66026de4d88b11c0b7d5175eb3
daniel.nichter@percona.com <daniel.nichter@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2009-03-29 15:05:38 +0000

    Patch EventAggregator with work-around for issue 332.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@3280 dfb901c2-3250-0410-b216-0b33211c9131

--- common/EventAggregator.pm
+++ common/EventAggregator.pm
@@ -519,7 +519,13 @@ sub calculate_statistical_metrics {
       $prev   =  $bucket;
    }
 
-   my $stddev   = sqrt (($sumsq - (($sum**2) / $n_vals)) / $n_vals);
+   # TODO: remove this once we can determine the real bug
+   # behind issue 332.
+   my $var = ($sumsq - (($sum**2) / $n_vals)) / $n_vals;
+   MKDEBUG && _d('var before sqrt:', $var);
+   $var = 0 if $var < 0;
+   my $stddev = sqrt($var);
+
    my $maxstdev = (($args->{max} || 0) - ($args->{min} || 0)) / 2;
    $stddev      = $stddev > $maxstdev ? $maxstdev : $stddev;
 
