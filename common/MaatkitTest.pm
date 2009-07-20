7d3da7ca1be7a687da4a9537110df25bdc15e89b
daniel@percona.com <daniel@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2009-07-20 16:42:17 +0000

    Add MaatkitText.pm.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@4200 dfb901c2-3250-0410-b216-0b33211c9131

---
+++ common/MaatkitTest.pm
@@ -0,0 +1,64 @@
+# This program is copyright 2009-@CURRENTYEAR@ Percona Inc.
+# Feedback and improvements are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2; OR the Perl Artistic License.  On UNIX and similar
+# systems, you can issue `man perlgpl' or `man perlartistic' to read these
+# licenses.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+# ###########################################################################
+# MaatkitTest package $Revision$
+# ###########################################################################
+package MaatkitTest;
+
+# These are common subs used in Maatkit test scripts.
+
+use strict;
+use warnings FATAL => 'all';
+
+use English qw(-no_match_vars);
+
+require Exporter;
+our @ISA         = qw(Exporter);
+our %EXPORT_TAGS = ();
+our @EXPORT      = qw();
+our @EXPORT_OK   = qw(
+   output
+   load_file
+);
+
+use constant MKDEBUG => $ENV{MKDEBUG};
+
+# This sub doesn't work yet because "mk_upgrade::main" needs to be ref somehow.
+sub output {
+   my $output = '';
+   open my $output_fh, '>', \$output
+      or BAIL_OUT("Cannot capture output to variable: $OS_ERROR");
+   select $output_fh;
+   eval { mk_upgrade::main(@_); };
+   close $output_fh;
+   select STDOUT;
+   return $EVAL_ERROR ? $EVAL_ERROR : $output;
+}
+
+sub load_file {
+   my ( $file ) = @_;
+   open my $fh, "<", $file or die "Cannot open $file: $OS_ERROR";
+   my $contents = do { local $/ = undef; <$fh> };
+   close $fh;
+   return $contents;
+}
+
+1;
+
+# ###########################################################################
+# End MaatkitTest package
+# ###########################################################################
