cfed3eedaa127e25873c4e7881d8fb8ae0486aed
daniel@percona.com <daniel@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2009-11-23 23:17:16 +0000

    Make to_string easier to use.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@5182 dfb901c2-3250-0410-b216-0b33211c9131

--- util/to_string.pl
+++ util/to_string.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 require '../common/MySQLProtocolParser.pm';
-my $str = MySQLProtocolParser::to_string(@ARGV);
+my $str = shift @ARGV;
+$str =~ s/\s+//g;
+my $str = MySQLProtocolParser::to_string($str);
 print "$str\n";
 exit;
