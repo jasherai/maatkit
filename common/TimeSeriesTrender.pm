d71f0f9458ed3893e03b827f304afd693d22de75
baron.schwartz <baron.schwartz@dfb901c2-3250-0410-b216-0b33211c9131>
2010-06-21 11:45:34 +0000

    fix header
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@6494 dfb901c2-3250-0410-b216-0b33211c9131

--- common/TimeSeriesTrender.pm
+++ common/TimeSeriesTrender.pm
@@ -1,4 +1,3 @@
-#!/usr/bin/perl
 # This program is copyright 2010-@CURRENTYEAR@ Percona Inc.
 # Feedback and improvements are welcome.
 #
@@ -16,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA.
 # ###########################################################################
-# FileIterator package $Revision$
+# TimeSeriesTrender package $Revision$
 # ###########################################################################
 package TimeSeriesTrender;
 
