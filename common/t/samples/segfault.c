fee7647a54e631f8d70f75f1308f2057f4867f51
daniel@percona.com <daniel@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2010-08-13 15:29:58 +0000

    Remove unused sample files.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@6897 dfb901c2-3250-0410-b216-0b33211c9131

--- common/t/samples/segfault.c
+++
@@ -1,8 +0,0 @@
-// Yes, the only purpose of this program is segfault.
-// See issue 135.
-#include <stdio.h>
-int main() {
-   fprintf(stderr, "Going to segfault...\n");
-   char *s = "thanks wikipedia";
-   *s = 'a';
-}
