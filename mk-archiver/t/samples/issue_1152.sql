9b1f5ce6575f195b6f1de78f27c725f139e25a31
daniel@percona.com <daniel@percona.com@dfb901c2-3250-0410-b216-0b33211c9131>
2011-02-26 23:15:56 +0000

    Update issue 1152 Use user's table for test.
    git-svn-id: http://maatkit.googlecode.com/svn/trunk@7303 dfb901c2-3250-0410-b216-0b33211c9131

--- mk-archiver/t/samples/issue_1152.sql
+++ mk-archiver/t/samples/issue_1152.sql
@@ -4,12 +4,18 @@ CREATE DATABASE issue_1152;
 CREATE DATABASE issue_1152_archive;
 
 USE issue_1152;
-CREATE TABLE t (
-   id  INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-   a   INT,
-   b   INT,
-   c   INT
-);
+CREATE TABLE `t` (
+  `LineTestID` bigint(19) unsigned NOT NULL AUTO_INCREMENT,
+  `JobCreatedTime` bigint(20) unsigned DEFAULT NULL,
+  `JobScheduledTime` bigint(20) unsigned DEFAULT NULL,
+  `JobStartTime` bigint(20) unsigned DEFAULT NULL,
+--  `JobFinishTime` bigint(20) unsigned DEFAULT NULL,
+--  `Priority` tinyint(3) unsigned DEFAULT NULL,
+--  `JobType` tinyint(3) unsigned DEFAULT NULL,
+--  `TerminationNumber` varchar(40) NOT NULL,
+  PRIMARY KEY (`LineTestID`)
+--  KEY `index1` (`JobScheduledTime`,`TerminationNumber`)
+) ENGINE=InnoDB;
 INSERT INTO issue_1152.t VALUES
    (null, 1, 2, 3),
    (null, 2, 2, 3),
@@ -24,8 +30,13 @@ INSERT INTO issue_1152.t VALUES
 
 USE issue_1152_archive;
 CREATE TABLE t (
-   id  INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-   a   INT,
-   b   INT,
-   c   INT
-);
+  `JobCreatedTime` bigint(20) unsigned DEFAULT NULL,
+  `JobScheduledTime` bigint(20) unsigned DEFAULT NULL,
+  `JobStartTime` bigint(20) unsigned DEFAULT NULL
+--  `JobFinishTime` bigint(20) unsigned DEFAULT NULL,
+--  `Priority` tinyint(3) unsigned DEFAULT NULL,
+--  `JobType` tinyint(3) unsigned DEFAULT NULL,
+--  `TerminationNumber` varchar(40) NOT NULL,
+--  PRIMARY KEY (`LineTestID`),
+--  KEY `index1` (`JobScheduledTime`,`TerminationNumber`)
+) ENGINE=InnoDB;
